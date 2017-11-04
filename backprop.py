03698a88307eacee1a28400a054c71ab8a09fe20
Simon Crase <simon@greenweaves.nz>
2017-11-04 16:53:53 +1300

    Generalize to multiple layers

--- backprop.py
+++ backprop.py
@@ -18,29 +18,22 @@ import math,numpy as np
 def sigmoid(z):
     return 1.0/(1.0+math.exp(-z))
 
-sigmoid_v=np.vectorize(sigmoid)
+def predict(Thetas, X,fn=np.vectorize(sigmoid)):
+    XX=X
+    m=len(X)
+    for Theta in Thetas:
+        XX=np.append(XX,np.ones((m,1)),axis=1)
+        XT=np.dot(Theta,XX)
+        Activation=fn(np.sum(XT,axis=1))
+        m=len(Activation)
+        XX=Activation.reshape(m,1)
+    return XX
 
-def predict(Theta1, Theta2, X):
-    m1=len(X)
-    XX=np.append(X,np.ones((m1,1)),axis=1)
-    XT=np.dot(Theta1,XX)
-    #XT1=np.sum(XT,axis=1)
-    A=sigmoid_v(np.sum(XT,axis=1))
-    #print (A)
-    m2=len(A)
-    #print (m2,np.ones((m2,1)))
-    XX2=np.append(A.reshape(m2,1),np.ones((m2,1)),axis=1)
-    #print (XX2)
-    XTT=np.dot(Theta2,XX2)
-    A2=sigmoid_v(np.sum(XTT,axis=1))
-    #print (A2)
-    return A2
 
 if __name__=='__main__':
     Theta1=np.array([[1,10,100,1000],[1,1,1,1],[1,1,1,1]])
     Theta2=np.array([[1,1,1],[1,1,1]])
-    #print(Theta1,Theta2)
     X0=np.array([1,2,3,4])
     m=len(X0)
     X2=X0.reshape((m,1))
-    print (predict(Theta1,Theta2,X2))
+    print (predict([Theta1,Theta2],X2))
