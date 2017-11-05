714bceefd117fd3258d54f76228a77bfad524765
Simon Crase <simon@greenweaves.nz>
2017-11-05 15:46:30 +1300

    Calculate first delta_w term (last row)

--- backprop.py
+++ backprop.py
@@ -22,12 +22,17 @@ def predict(Thetas, Inputs,fn=np.vectorize(sigmoid)):
     X=Inputs
     m=len(X)
     derivatives=[]
+    Xs=[]
     for Theta in Thetas:
+        Xs.append(X)
         X_with_bias=np.append(X,[1],axis=0)
-        X=fn(np.dot(X_with_bias,Theta))
+        #print ('X_with_bias',X_with_bias)
+        product=np.dot(X_with_bias,Theta)
+        #print ('P',product)
+        X=fn(product)
         derivatives.append([g*(1-g) for g in X])
         m=len(X)       
-    return (X,derivatives)
+    return (X,derivatives,Xs)
 
 def create_thetas(layer_spec):
     def create_theta(a,b):
@@ -39,14 +44,13 @@ def create_thetas(layer_spec):
 def error(target,output):
     return 0.5*sum([(t-o)*(t-o) for t,o in zip(target,output)])
     
-def train(Thetas, Inputs,Outputs,fn=np.vectorize(sigmoid)):
-    a_k,derivatives=predict(Thetas, Inputs,fn=fn)
-    delta_k=[a - o for (a,o) in zip(a_k,Outputs)]
-    print (delta_k)
-    Theta2=Thetas[-1]
-    print (Theta2)
-    print (derivatives[-1])
-    
+def delta_weights(target,output,derivs,Xs):
+    errors=np.subtract(output,target)
+    for D,X in zip(derivs[::-1],Xs[::-1]):
+        hadamard=np.multiply(D,errors)
+        delta = np.outer(X,hadamard)
+        return delta
+        
 if __name__=='__main__':
     class TestEvaluation(unittest.TestCase):
         '''Tests based on https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/'''
@@ -54,10 +58,18 @@ if __name__=='__main__':
             '''A test to verify correct calculation of feedforward network'''
             Theta1=np.array([[0.15,0.25],[0.2,0.3],[0.35,0.35]])
             Theta2=np.array([[0.4,0.50],[0.45,0.55],[0.6,0.6]])
-            z,_=predict([Theta1,Theta2],[0.05,0.1])
+            z,_,_=predict([Theta1,Theta2],[0.05,0.1])
             self.assertAlmostEqual(0.75136507,z[0],delta=0.0000001)
             self.assertAlmostEqual(0.772928465,z[1],delta=0.0000001)
         def test_error(self):
             self.assertAlmostEqual(0.2983171109,error([0.01,0.99],[0.75136507,0.772928465]),delta=0.0001)
-    
+        def test_errors_output(self):
+            Theta1=np.array([[0.15,0.25],[0.2,0.3],[0.35,0.35]])
+            Theta2=np.array([[0.4,0.50],[0.45,0.55],[0.6,0.6]])
+            z,derivs,Xs=predict([Theta1,Theta2],[0.05,0.1]) 
+            #print (derivs)
+            delta=delta_weights(np.array([0.01,0.99]),z,derivs,Xs)
+            for i in range(2):
+                for j in range(2):
+                    print (Theta2[i][j]-0.5*delta[i][j])
     unittest.main()
