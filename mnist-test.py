3dc9a91677b0a3fc8394e944ef120a8e92a8cfd5
Simon Crase <simon@greenweaves.nz>
2017-11-07 10:24:44 +1300

    Verify Theta using test data

--- mnist-test.py
+++ mnist-test.py
@@ -2,7 +2,11 @@ import backprop as bp, random
 from mnist import MNIST
 
 # http://yann.lecun.com/exdb/mnist/
-
+def digit(l,threshold=0.9):
+        for i in range(len(l)):
+                if l[i]>threshold:
+                        return i
+                
 mndata = MNIST(r'.\data')
 
 images, labels = mndata.load_training()
@@ -20,13 +24,21 @@ def data_gen():
                 yield target,[0 if ii<64 else 1 for ii in images[i]]
                 i+=1
     
-Thetas,_=bp.gradient_descent(Thetas,data_source=data_gen(),eta=0.2)
+Thetas,_=bp.gradient_descent(Thetas,data_source=data_gen(),eta=0.2,print_interval=1000)
 
 images_test, labels_test = mndata.load_testing()
 
+
+                
+
 for i in range(len(labels_test)):
+        total_errors=0
         target=[0]*10
         target[labels_test[i]]=1        
         z,_,_=bp.predict(Thetas,[0 if ii<64 else 1 for ii in images_test[i]])
         err=bp.error(target,z)
-        print (labels_test[i],err,z)       
+        dd=digit(z)
+        print (labels_test[i],dd,err)
+        if dd!=labels_test[i]:
+                total_errors+=1
+print ('Error count ={0}'.format(total_errors))
