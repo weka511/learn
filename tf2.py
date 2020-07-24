2e35ef30bfad5c719897a99b5c889d63ce577b4b
Simon Crase <simon@greenweaves.nz>
2020-07-24 12:21:16 +1200

    Now reasonabley accurate

--- tf2.py
+++ tf2.py
@@ -16,41 +16,35 @@ x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 print (x_train.shape) # (60000, 28, 28)
 print (y_train.shape) #(60000, )
+
 input_shape = (28, 28, 1)
 # Build model
 model = Sequential([
-  Convolution2D(28, 3, 3, activation='relu', input_shape=input_shape),
+  Convolution2D(32, 3, 3, activation='relu', input_shape=input_shape,padding='same'),
   MaxPooling2D(pool_size=(2,2)),
-  #Dropout(0.25),
+  Convolution2D(64, (3, 3), activation='relu',padding='same'),
+  MaxPooling2D(pool_size=(2,2)),
+  Convolution2D(64, (3, 3), activation='relu',padding='same'),
   Flatten(), # Flattens the input. Does not affect the batch size.
-  Dense(128, activation=tf.nn.relu), # Just your regular densely-connected NN layer.
-  Dropout(0.2),                  # The Dropout layer randomly sets input units to 0 with a
+  Dense(64, activation=tf.nn.relu), # Just your regular densely-connected NN layer.
+  #Dropout(0.2),                  # The Dropout layer randomly sets input units to 0 with a
                                                  ##frequency of rate at each step during training time,
                                                  ## which helps prevent overfitting. Inputs not set to 0 
                                                  ## are scaled up by 1/(1 - rate) such that the sum over
                                                  ## all inputs is unchanged.
- Dense(10, activation=tf.nn.softmax)
+ Dense(10)
 ])
+model.summary()
 
-# For each example the model returns a vector of "logits" or "log-odds" scores, one for each class.
-#predictions = model(x_train[:1]).numpy()
-# The tf.nn.softmax function converts these logits to "probabilities" for each class: 
-#tf.nn.softmax(predictions).numpy()
-# The losses.SparseCategoricalCrossentropy loss takes a vector of logits 
-# and a True index and returns a scalar loss for each example. This loss is equal to the negative
-# log probability of the true class: It is zero if the model is sure of the correct class.
-#loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-#print (loss_fn(y_train[:1], predictions).numpy())
-
-# We specify the training configuration (optimizer, loss, metrics):
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',#loss_fn,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 # Train the model by slicing the data into "batches" of size "batch_size", and repeatedly iterating
 # over the entire dataset for a given number of "epochs".
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=5, 
+                    validation_data=(x_test,y_test))
 
 #  check the models performance
 
