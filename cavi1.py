b2455bd94cedad60f14c5551d822669aa309f0fc
Simon Crase <simon@greenweaves.nz>
2020-08-15 10:52:20 +1200

    #3 Added Converges, but overstimates sigma

--- cavi1.py
+++ cavi1.py
@@ -33,7 +33,7 @@ def create_data(n=100,sigma=1,mu=0):
 #
 # I have borrowed some ideas from 
 # https://suzyahyah.github.io/bayesian%20inference/machine%20learning/variational%20inference/2019/03/20/CAVI.html
-def cavi(xs,N=25,tolerance=0.01):
+def cavi(xs,N=25,N0=5,tolerance=0.000001):
     def calc_elbo():
         x_mu_sq     = sum([(x-mu)*(x-mu) for x in xs]) 
         cov_term    = -0.5 / sigma
@@ -45,42 +45,48 @@ def cavi(xs,N=25,tolerance=0.01):
         
         return log_p_x + log_p_mu + log_p_sigma - log_q_mu - log_q_sigma
     
+    xbar    = np.mean(xs)
+    
     elbos   = []
+    # hyperpriors
     mu0     = 0
-    sigma0  = 2
     alpha0  = 0
     beta0   = 0
+    sigma0  = 2
+
     mu_n    = mu0
     alpha_n = alpha0
     beta_n  = beta0
     
     sigma   = sigma0
     mu      = random.gauss(mu0,sigma)
-    xbar    = sum(xs)/len(xs)
+
     for _ in range(N):
-        if len(elbos)>5 and abs(elbos[-1]-elbos[-2])<tolerance: return (True,mu,sigma,elbos)
+        if len(elbos)>N0 and abs(elbos[-1]-elbos[-2])<tolerance: return (True,mu,sigma,elbos)
         
         mu_n    = (sum(xs) + mu0)/(len(xs)+1)
         mu      = mu_n
         alpha_n = alpha0 + (len(xs)+1)/2
-        beta_n  =                                                            \
-            beta0 +  0.5*sum(x*x for x in xs) - xbar * sum(x for x in xs) +  \
-            + ((len(xs)+1)/2)*(sigma/len(xs)+xbar*xbar)- mu0*xbar + 0.5*mu0*mu0
+        beta_n  = beta0 +  0.5*sum(x**2 for x in xs) - xbar * sum(x for x in xs)   \
+                  + ((len(xs)+1)/2)*(sigma/len(xs) + xbar**2)- mu0*xbar + 0.5*mu0*mu0
         sigma = (beta_n-1)/alpha_n
         # compute elbo
         elbos.append(calc_elbo())
-        print (mu,sigma,elbos[-1]) 
+ 
     return (False,mu,sigma,elbos)
 
 if __name__=='__main__':
     import matplotlib.pyplot as plt
     random.seed(1)
-    plt.subplot(211)
-    xs = create_data(mu=10)
-    plt.hist(xs)
-    plt.subplot(212)
-    _,mu,sigma,elbos = cavi(xs,N=5)
-    print (mu,sigma) 
-    plt.plot(range(len(elbos)),elbos)
+    xs = create_data(mu=0.5,sigma=0.5,n=1000)
+    _,mu,sigma,elbos = cavi(xs,N0=20)
+    
+    plt.figure(figsize=(10,10))
+    ax1=plt.subplot(211)
+    ax1.hist(xs) 
+    ax1.set_title (f'Data: mean(xs)={np.mean(xs):.3f}, std(xs)={np.std(xs):.3f}')
+    ax2=plt.subplot(212)
+    ax2.set_title (f'ELBO: mu={mu:.3f}, sigma={sigma:.3f}')
+    ax2.plot(elbos)
     plt.show()
     
