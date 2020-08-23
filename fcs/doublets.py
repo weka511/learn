dab34d3250c4f3c5f4b7f66654466acf3c051500
Simon Crase <simon@greenweaves.nz>
2020-08-23 19:05:08 +1200

    #6: created skelton code

---
+++ fcs/doublets.py
@@ -0,0 +1,75 @@
+# Copyright (C) 2020 Greenweaves Software Limited
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
+import gcps,fcsparser
+
+if __name__=='__main__':
+    import os, re, argparse,sys,matplotlib.pyplot as plt
+    from matplotlib import rc
+    rc('text', usetex=True)
+    
+    parser   = argparse.ArgumentParser('Fit Gaussian Mixture Model to GCP wells')
+    parser.add_argument('-r','--root',
+                        default=r'\data\cytoflex\Melbourne',
+                        help='Root for fcs files')
+    parser.add_argument('-p','--plate',
+                        default='all',
+                        nargs='+',
+                        help='Name of plate to be processed (omit for all)')
+    parser.add_argument('-w','--well', 
+                        default=['G12','H12'],
+                        nargs='+',
+                        help='Names of wells to be processed (omit for all)')
+    parser.add_argument('-s', '--show',
+                        default=False,
+                        action='store_true',
+                        help='Display graphs')
+    
+    args   = parser.parse_args()
+    show   = args.show or args.plate!='all'
+
+    for root, dirs, files in os.walk(args.root):
+        path  = root.split(os.sep)
+        match = re.match('.*(((PAP)|(RTI))[A-Z]*[0-9]+[r]?)',path[-1])
+        if match:
+            plate = match.group(1)
+            if args.plate=='all' or plate in args.plate:
+                for file in files:
+                    if re.match('.*[GH]12.fcs',file):
+                        path     = os.path.join(root,file)
+                        meta, df = fcsparser.parse(path, reformat_meta=True)
+                        
+                        date     = meta['$DATE']
+                        tbnm     = meta['TBNM']
+                        btim     = meta['$BTIM']
+                        etim     = meta['$ETIM']
+                        cyt      = meta['$CYT']
+                        df1      = gcps.purge_outliers(df)
+                        well     = gcps.get_well_name(tbnm)
+ 
+                        if well in args.well:
+                            print (f'{plate} {well}')
+  
+                            plt.figure(figsize=(10,10))
+                            plt.suptitle(f'{plate} {well}')
+                            
+                            ax1 = plt.subplot(2,2,1)
+                            ax1.scatter(df1['FSC-H'],df1['SSC-H'],s=1,c='g')
+                            ax1.set_xlabel('FSC-H')
+                            ax1.set_ylabel('SSC-H')
+    
+    if show:
+        plt.show()  
+        
