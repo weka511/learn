d9278c13f89ce7b5df577cdfd06f120f578ec723
Simon Crase <simon@greenweaves.nz>
2020-08-28 09:48:13 +1200

    #10: save plot files

--- fcs/sizes.py
+++ fcs/sizes.py
@@ -18,12 +18,15 @@
 # https://www.ics.uci.edu/~smyth/courses/cs274/notes/EMnotes.pdf
 
 
-import  re, argparse,sys,em,os,fcsparser,fcs,matplotlib.pyplot as plt,numpy as np
+import  re, argparse,sys,em,os,fcsparser,fcs,matplotlib.pyplot as plt,numpy as np,time
 from matplotlib import rc
 from mpl_toolkits.mplot3d import Axes3D
 
 rc('text', usetex=True)
 cmap     = plt.cm.get_cmap('RdYlBu')
+
+d = 3
+
 parser   = argparse.ArgumentParser('Fit Gaussian Mixture Model to GCP wells')
 
 parser.add_argument('-r','--root',
@@ -37,7 +40,18 @@ parser.add_argument('-w','--well',
                     default=[],
                     nargs='+',
                     help='Names of wells to be processed (omit for all)')
+parser.add_argument('-K','--K',
+                    default=[6],
+                    type=int,
+                    nargs='+')
+
+parser.add_argument('-s', '--show',
+                    default=False,
+                    action='store_true',
+                    help='Display graphs')
+
 args   = parser.parse_args()
+
 for root, dirs, files in os.walk(args.root):
     path  = root.split(os.sep)
     match = re.match('.*(((PAP)|(RTI))[A-Z]*[0-9]+[r]?)',path[-1])
@@ -55,35 +69,53 @@ for root, dirs, files in os.walk(args.root):
                     etim     = meta['$ETIM']
                     cyt      = meta['$CYT']
                     well     = fcs.get_well_name(tbnm)
-                    df1      = df[(0               < df['FSC-H']) & \
+                    
+                    df1      = df[(200000          < df['FSC-H']) & \
                                   (df['FSC-H']     < 1000000  )   & \
-                                  (0               < df['SSC-H']) & \
+                                  (75000           < df['SSC-H']) & \
                                   (df['SSC-H']     < 1000000)     & \
-                                  (df['FSC-Width'] < 2000000)]
+                                  (df['FSC-Width'] < 2000)]
                     
                     if well in args.well or len(args.well)==0:
-                        print (f'{plate} {well}') 
-                        plt.figure(figsize=(10,10))
-                        plt.suptitle(f'{plate} {well}')
-
+ 
+                        start_time = time.time()                        
+ 
                         xs     = df1['FSC-H'].values
                         ys     = df1['SSC-H'].values
                         zs     = df1['FSC-Width'].values  
-                        ax1    = plt.subplot(3,3,1, projection='3d')
-                        plt.colorbar(ax1.scatter(xs,ys,zs,s=1,c=zs,cmap=cmap)) 
-                        K      = 6
-                        mus    = em.get_mus(xs,ys,zs)
-                        alphas = [1/K for _ in range(K)]
-                        Sigmas = [np.cov([xs,ys,zs],rowvar=True) for _ in range(K)]
-                    
-                        maximized,likelihoods,ws,alphas,mus,Sigmas = \
-                            em.maximize_likelihood(
-                                xs,ys,zs,
-                                mus=mus,
-                                Sigmas=Sigmas,
-                                alphas=alphas,
-                                K=K)
-                        for k in range(K):
-                            axk    = plt.subplot(3,3,k+2, projection='3d')
-                            plt.colorbar(axk.scatter(xs,ys,zs,s=1,c=ws[k],cmap=cmap)) 
-plt.show()
+   
+                        for K in args.K:
+                            try:
+                                mus    = em.get_mus(xs,ys,zs,K=K,min_separation=10000000)
+                                alphas = [1/K for _ in range(K)]
+                                #Sigmas = [np.cov([xs,ys,zs],rowvar=True) for _ in range(K)]   
+                                Sigma = [[0 for _ in range(d)] for _ in range(d)]
+                                Sigma[0][0]= 1e10
+                                Sigma[1][1]= 1e10
+                                Sigma[2][2]= 1e4
+                                
+                                Sigmas = [Sigma for _ in range(K)]  
+                                maximized,likelihoods,ws,alphas,mus,Sigmas = \
+                                    em.maximize_likelihood(
+                                        xs,ys,zs,
+                                        mus    = mus,
+                                        Sigmas = Sigmas,
+                                        alphas = alphas,
+                                        K      = K)
+                                plt.figure(figsize=(10,10))                       
+                                for k in range(K):
+                                    axk    = plt.subplot(3,3,k+1, projection='3d')
+                                    axk.scatter(xs,ys,zs,s=1,c=ws[k],cmap=cmap)
+                                elapsed_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
+                                plt.suptitle(f'{plate} {well} K={K}: elapsed {elapsed_time}')
+                                plt.savefig(
+                                    fcs.get_image_name(
+                                        script = os.path.basename(__file__).split('.')[0],
+                                        plate  = plate,
+                                        well   = well,
+                                        K      = K))                                
+                                print (f'{plate} {well} K={K}: elapsed {elapsed_time}')                        
+                            except:
+                                print (f'{plate} {well} K={K}: failed: {sys.exc_info()[0]}') 
+if args.show:
+    plt.show()
