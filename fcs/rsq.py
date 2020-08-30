6fe7c6cd2bb500eff3d53d728577a388a4a4ce97
Simon Crase <simon@greenweaves.nz>
2020-08-30 15:59:09 +1200

    rsq file can have differnt name; tidied histogram

--- fcs/rsq.py
+++ fcs/rsq.py
@@ -13,14 +13,25 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
-import re, matplotlib.pyplot as plt
+import re
+import matplotlib.pyplot as plt
+import argparse
+from matplotlib import rc
+rc('text', usetex=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('file')
+args = parser.parse_args()
 
-with open('rsq.txt') as f:
+with open(args.file) as f:
     r_sqs = []
     for line in f:
         m = re.search(r'r_value=([01]\.[0-9]+)',line.strip())
         if m:
             r_sqs.append(float(m.group(1)))
-    plt.hist(r_sqs,bins='fd')
+            
+    plt.hist(r_sqs,bins='fd',color='blue')
+    plt.title(args.file)
+    plt.xlabel(r'$r^2$')
+    plt.ylabel('N')
     plt.show()
         
