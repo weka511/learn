d44c8e9cd014f34b3b94363997ed1cc4287b2fae
Simon Crase <simon@greenweaves.nz>
2020-09-13 15:18:11 +1200

    CNN demo

---
+++ LeNet5.py
@@ -0,0 +1,29 @@
+# https://towardsdatascience.com/understanding-and-implementing-lenet-5-cnn-architecture-deep-learning-a2d531ebc342
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+(train_x, train_y), (test_x, test_y) = keras.datasets.mnist.load_data()
+train_x = train_x / 255.0
+test_x = test_x / 255.0
+train_x = tf.expand_dims(train_x, 3)
+test_x = tf.expand_dims(test_x, 3)
+val_x = train_x[:5000]
+val_y = train_y[:5000]
+
+lenet_5_model = keras.models.Sequential([
+    keras.layers.Conv2D(6, kernel_size=5, strides=1,  activation='tanh', input_shape=train_x[0].shape, padding='same'), #C1
+    keras.layers.AveragePooling2D(), #S2
+    keras.layers.Conv2D(16, kernel_size=5, strides=1, activation='tanh', padding='valid'), #C3
+    keras.layers.AveragePooling2D(), #S4
+    keras.layers.Flatten(), #Flatten
+    keras.layers.Dense(120, activation='tanh'), #C5
+    keras.layers.Dense(84, activation='tanh'), #F6
+    keras.layers.Dense(10, activation='softmax') #Output layer
+])
+
+lenet_5_model.compile(optimizer='adam', loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+
+lenet_5_model.fit(train_x, train_y, epochs=5, validation_data=(val_x, val_y))
+
+lenet_5_model.evaluate(test_x, test_y)
