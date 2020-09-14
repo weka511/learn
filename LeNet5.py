b8a89d7d08ead6209878b01c2b8ea6267d2629ca
Simon Crase <simon@greenweaves.nz>
2020-09-14 21:53:13 +1200

    Pickup training again from checkpoint

--- LeNet5.py
+++ LeNet5.py
@@ -23,8 +23,10 @@ from keras.models import Sequential
 from keras.layers import Conv2D,AveragePooling2D,Flatten,Dense
 from tensorflow.keras.callbacks import ModelCheckpoint
 import numpy as np
-import os
-import argparse
+
+# create_data
+#
+# Read training and test data
 
 def create_data():
     (train_x, train_y), (test_x, test_y) = keras.datasets.mnist.load_data()
@@ -36,71 +38,75 @@ def create_data():
     val_y                                = train_y[:5000]
     return (train_x, train_y), (test_x, test_y), (val_x,val_y)
 
-def create_model(train_x):
-    return Sequential([
-            Conv2D(6, kernel_size=5, strides=1,  activation='tanh', input_shape=train_x[0].shape, padding='same'), #C1
-            AveragePooling2D(), #S2
-            Conv2D(16, kernel_size=5, strides=1, activation='tanh', padding='valid'), #C3
-            AveragePooling2D(), #S4
-            Flatten(), #Flatten
-            Dense(120, activation='tanh'), #C5
-            Dense(84, activation='tanh'), #F6
-            Dense(10, activation='softmax') #Output layer
-    ])
+# create_model
+#
+# Create Neuaral Network model and compile it
+#
+# Parameters:
+#     train_x    Used to assign size to first layer
 
-def ch(filepath,save_freq=5):
-    tf.keras.callbacks.ModelCheckpoint(filepath=filepath,
-                                       save_weights_only=True,
-                                       save_freq=save_freq,
-                                       verbose=1)    
+def create_model(train_x):
+    product = Sequential([
+        Conv2D(6, kernel_size=5, strides=1,  activation='tanh', input_shape=train_x[0].shape, padding='same'), #C1
+        AveragePooling2D(), #S2
+        Conv2D(16, kernel_size=5, strides=1, activation='tanh', padding='valid'), #C3
+        AveragePooling2D(), #S4
+        Flatten(), #Flatten
+        Dense(120, activation='tanh'), #C5
+        Dense(84, activation='tanh'), #F6
+        Dense(10, activation='softmax') #Output layer
+    ])
+    product.compile(optimizer='adam',
+                   loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+                   metrics=[tf.metrics.SparseCategoricalAccuracy()])
+
+    return product
+
 if __name__=='__main__':
+    import argparse
+    import os
+    
+    print(f'Using Tensorflow {tf.version.VERSION}')    
     path   = os.path.join(os.getenv('APPDATA'),'LeNet5')
     parser = argparse.ArgumentParser('Convolutional Neural Net based on LeNet-5')
     parser.add_argument('action',   choices=['train','test', 'continue'],       help = 'Train or test')
     parser.add_argument('--checkpoint',                       default='checkpoint', help = 'Name of file to save and restore network')
     parser.add_argument('--path',                             default=path,     help = 'Path of file used to save and restore network')
     parser.add_argument('--epochs', type=int,                 default=5,       help = 'Number of epochs for training')
-    args = parser.parse_args()
+    args        = parser.parse_args()
     
-    checkpoint = os.path.join(args.path, args.checkpoint, 'cp-{epoch:04d}.ckpt')
+    checkpoint  = os.path.join(args.path, args.checkpoint, 'cp-{epoch:04d}.ckpt')
+    
+    cp_callback = ModelCheckpoint(filepath   = checkpoint,
+                                 save_weights_only = True,
+                                 save_freq         = 'epoch',
+                                 verbose           = 1)
     
     if args.action == 'train':
         (train_x, train_y), (test_x, test_y), (val_x,val_y) = create_data()
         
         model = create_model(train_x)
         
-        model.compile(optimizer='adam', loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
-        
         model.save_weights(checkpoint.format(epoch=0))
         
         model.fit(train_x, train_y,
                   epochs          = args.epochs,
                   validation_data = (val_x, val_y),
-                  callbacks       = [ModelCheckpoint(filepath   = checkpoint,
-                                             save_weights_only = True,
-                                             save_freq         = 'epoch',
-                                             verbose           = 1)])
-        #save_path = os.path.join(args.path,args.file)
-        
-        #tf.saved_model.save(model, save_path) # https://www.tensorflow.org/guide/saved_model
-        #print (f'Saved to {save_path}')
+                  callbacks       = [cp_callback])
+  
     
     if args.action == 'test':
         latest = tf.train.latest_checkpoint(os.path.dirname(checkpoint))
         print (f'Latest checkpoint: {latest}')
         _, (test_x, test_y), _ = create_data()
 
-        model              = create_model(test_x)#  os.path.join(args.path,args.file)
+        model              = create_model(test_x)
        
-        model.load_weights(latest) #                               = tf.keras.models.load_model(load_path)
+        model.load_weights(latest) 
         print (f'Loaded {latest}')
-        model.compile()
-        print ('Check its architecture')
-        model.summary()
         
-        print ('Testing')
-        loss = model.evaluate(test_x, test_y, verbose=2)
-        print (f'Loss')
+        loss,accuracy = model.evaluate(test_x, test_y, verbose=2)
+        print (f'Tested: loss={loss}, accuracy={accuracy}')
        
     if args.action=='continue':
         latest = tf.train.latest_checkpoint(os.path.dirname(checkpoint))
@@ -108,25 +114,12 @@ if __name__=='__main__':
         (train_x, train_y), (test_x, test_y), (val_x,val_y) = create_data()
         
         model              = create_model(train_x)
-        model.compile()     
+    
         model.load_weights(latest) 
         print (f'Loaded {latest}')
-        print ('Check its architecture')
-        model.summary()
+ 
         model.fit(train_x, train_y,
                   epochs=args.epochs,
                   validation_data=(val_x, val_y),
-                  callbacks=[ch])        
-        #print ('Check its architecture')
-        #model.summary()              
-        #load_path                            = os.path.join(args.path,args.file)
-        #model                            = tf.keras.models.load_model(load_path)
-        #print (f'Loaded {load_path}')
-        #print ('Check its architecture')
-        #model.summary()    
-        
-        #model.fit(train_x, train_y, epochs=args.epochs, validation_data=(val_x, val_y))
-        #save_path = os.path.join(args.path,args.file)
-        
-        #tf.saved_model.save(model, save_path) # https://www.tensorflow.org/guide/saved_model
-        #print (f'Saved to {save_path}')    
+                  callbacks=[cp_callback])        
+   
