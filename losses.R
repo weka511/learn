3336853c79d1b17b88be62f950d9f5ec93832e40
Simon Crase <simon@greenweaves.nz>
2020-09-15 11:50:56 +1200

    Tidied up start of script

--- losses.R
+++ losses.R
@@ -1,3 +1,23 @@
+# Copyright (C) 2020 Greenweaves Software Limited
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
+setwd("~/../learn")
+rm(list=ls())
+cat("\014") 
+if(!is.null(dev.list())) dev.off()
+
 df<-read.csv('training.txt')
 
 plot(df$epoch,df$loss,type="o", col="blue", pch="o", lty=1,xlab="Epoch",ylab="Loss")
