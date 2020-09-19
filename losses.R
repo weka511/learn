810b6bea9a72ddfe0dd2293004c333e3e9233fec
Simon Crase <simon@greenweaves.nz>
2020-09-20 11:05:00 +1200

    Plot logfiles for continue - epochs now handled correctly

--- losses.R
+++ losses.R
@@ -21,18 +21,49 @@ if(!is.null(dev.list())) dev.off()
 plot_results<-function(name) {
   df<-read.csv(name)
   
-  plot(df$epoch,df$loss,type="o", col="blue", pch="o", lty=1,xlab="Epoch",ylab="Loss")
-  points(df$epoch,df$val_loss, col="red", pch="*")
-  lines(df$epoch,df$val_loss, col="red",lty=2)
-  legend(x='topleft', legend=c("Loss", "Validation Loss"),
-         col=c("blue","red"), lty=1:2, cex=0.8)
+  plot(rownames(df),
+       df$loss,
+       type="o",
+       col="blue",
+       pch="o",
+       lty=1,
+       xlab="Epoch",
+       ylab="Loss")
+  points(rownames(df),
+         df$val_loss,
+         col="red",
+         pch="*")
+  lines(rownames(df),
+        df$val_loss,
+        col="red",
+        lty=2)
+  legend(x='topleft', 
+         legend=c("Loss", "Validation Loss"),
+         col=c("blue","red"),
+         lty=1:2, cex=0.8)
   title(name)
   
-  plot(df$epoch,df$sparse_categorical_accuracy,type="o", col="blue", pch="o", lty=1,xlab="Epoch",ylab="Accuracy")
-  points(df$epoch,df$val_sparse_categorical_accuracy, col="red", pch="*")
-  lines(df$epoch,df$val_sparse_categorical_accuracy, col="red",lty=2)
-  legend(x='bottomright', legend=c("Accuracy", "Validation Accuracy"),
-         col=c("blue","red"), lty=1:2, cex=0.8)
+  plot(rownames(df),
+       df$sparse_categorical_accuracy,
+       type="o",
+       col="blue",
+       pch="o",
+       lty=1,
+       xlab="Epoch",
+       ylab="Accuracy")
+  points(rownames(df),
+         df$val_sparse_categorical_accuracy,
+         col="red",
+         pch="*")
+  lines(rownames(df),
+        df$val_sparse_categorical_accuracy,
+        col="red",
+        lty=2)
+  legend(x='bottomright',
+         legend=c("Accuracy", "Validation Accuracy"),
+         col=c("blue","red"),
+         lty=1:2,
+         cex=0.8)
   title(name)
   
 }
