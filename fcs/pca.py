08a884190354510669b9562e323d590b9eadb345
Simon Crase <simon@greenweaves.nz>
2020-09-24 18:38:51 +1200

    Reformatted code

--- fcs/pca.py
+++ fcs/pca.py
@@ -25,28 +25,30 @@ def normalize(df,key):
     norm  = [(x-mu)/sigma for x in df[key]]
     return norm,mu,sigma
 
-def PCA(data):
+def covariance(data):
     d       = len(data)
-    m       = len(data[0])
     Cov     = np.zeros([d,d])
     for i in range(d):
         for j in range(i,d):
-            Cov[i][j] = sum([x*y for (x,y) in zip(data[i],data[j]) ]) / m
-            if not i==j:
+            Cov[i][j] = sum([x*y for (x,y) in zip(data[i],data[j]) ]) / len(data[0])
+            if i<j:
                 Cov[j][i]=Cov[i][j]
-    U,_,_ = np.linalg.svd(Cov)
+    return Cov
+
+def PCA(data):
+    U,_,_ = np.linalg.svd(covariance(data))
     return np.matmul(U.transpose(),data)
     
 if __name__=='__main__':
-    meta,df = fcsparser.parse(r'C:\data\cytoflex\Melbourne\PAP15100054\01-Tube-A12.fcs')
-    df1     = fcs.gate_data(df)
-    data    = np.zeros([3,df1.shape[0]])
-    fsc_h,mu_fsc_h,sigma_fsc_h = normalize(df1,'FSC-H')
-    ssc_h,mu_ssc_h,sigma_ssc_h = normalize(df1,'SSC-H')
-    fsc_w,mu_fsc_w,sigma_fsc_w = normalize(df1,'FSC-Width')
-    data[0] = [x for x in fsc_h]
-    data[1] = [x for x in ssc_h]
-    data[2] = [x for x in fsc_w]
-    dd      = PCA(data)
+    meta,df   = fcsparser.parse(r'C:\data\cytoflex\Melbourne\PAP15100054\01-Tube-A12.fcs')
+    df1       = fcs.gate_data(df,nsigma=2)
+    data      = np.zeros([3,df1.shape[0]])
+    fsc_h,_,_ = normalize(df1,'FSC-H')
+    ssc_h,_,_ = normalize(df1,'SSC-H')
+    fsc_w,_,_ = normalize(df1,'FSC-Width')
+    data[0]   = [x for x in fsc_h]
+    data[1]   = [x for x in ssc_h]
+    data[2]   = [x for x in fsc_w]
+    dd        = PCA(data)
     plt.scatter(dd[0],dd[1],s=1)
     plt.show()
