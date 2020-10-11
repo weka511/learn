51094230bd88edd744fb45319d37f5f09e1bf4d3
Simon Crase <simon@greenweaves.nz>
2020-10-12 10:36:35 +1300

    Issue #2: minor adjustment to data generation + command line params

--- cavi3.py
+++ cavi3.py
@@ -21,6 +21,7 @@
 #  Coordinate Ascent Mean-Field Variational Inference (CAVI) after
 #  David M. Blei, Alp Kucukelbir & Jon D. McAuliffe (2017) Variational Inference: A Review for Statisticians
 
+import argparse
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,20 +29,20 @@ import os
 import random
 import scipy.stats as stats
 
-def create_data(mu,n=1000,sigma=1):
+def create_data(mu,n=1000,sigmas=[]):
     def create_datum():
         i = random.randrange(len(mu))
-        return (i,random.gauss(mu[i],sigma))
+        return (i,random.gauss(mu[i],sigmas[i]))
     return list(zip(*[create_datum() for _ in range(n)]))
  
 # cavi
 #
 # Perform Coordinate Ascent Mean-Field Variational Inference
 #
-# I have borrowed some ideas from see https://zhiyzuo.github.io/VI/
-def cavi(x,k=6,N=25,sigma=1,tolerance=1e-6):
+# I have borrowed some ideas from Zhiya Zuo's blog--https://zhiyzuo.github.io/VI/
+def cavi(x,K=3,N=25,sigmas=1,tolerance=1e-6):
     def get_uniform_vector():
-        sample = [random.random() for _ in range(k)]
+        sample = [random.random() for _ in range(K)]
         return [s/sum(sample) for s in sample]
        
     # calcELBO
@@ -57,18 +58,18 @@ def cavi(x,k=6,N=25,sigma=1,tolerance=1e-6):
         return log_p_x + log_p_mu + log_p_sigma - log_q_mu - log_q_sigma
     
     phi   = [get_uniform_vector() for _ in x]
-    m     = [random.gauss(np.mean(x),sigma) for _ in range(k)]
-    s     = [random.random()*np.std(x) for _ in range(k)]
+    m     = [random.gauss(np.mean(x),sigma) for _ in range(K)]
+    s     = [random.random()*np.std(x) for _ in range(K)]
     ELBOs = [1,2]
     
     while (abs(ELBOs[-1]/ELBOs[-2]-1)>tolerance):
         # update cluster assignment
-        phi_update = [[math.exp(-0.5*m[_k]*m[_k] + s[_k]*s[_k] + x[i]*m[_k]) for _k in range(k)] for i in range(len(x))]
-        denoms     = [sum([phi_update[i][_k] for _k in range(k)] ) for i in range(len(x))]
-        phi        = [[phi_update[i][_k]/denoms[i] for _k in range(k)] for i in range(len(x))]
+        phi_update = [[math.exp(-0.5*m[k]*m[k] + s[k]*s[k] + x[i]*m[k]) for k in range(K)]  for i in range(len(x))]
+        denoms     = [sum([phi_update[i][k]                             for k in range(K)]) for i in range(len(x))]
+        phi        = [[phi_update[i][k]/denoms[i]                       for k in range(K)]  for i in range(len(x))]
         # update variational density
-        numerator   = [sum([phi[i][_k]*x[i] for i in range(len(x))]) for _k in range(k)]
-        denominator = [(1/(sigma*sigma))+sum([phi[i][_k] for i in range(len(x))]) for _k in range(k)]
+        numerator   = [sum([phi[i][k]*x[i]              for i in range(len(x))]) for k in range(K)]
+        denominator = [(1/(sigma*sigma))+sum([phi[i][k] for i in range(len(x))]) for k in range(K)]
         m           = [n/d for (n,d) in zip(numerator,denominator)]
         s           = [1/d for d in denominator]
         print (m,s)
@@ -77,41 +78,51 @@ def cavi(x,k=6,N=25,sigma=1,tolerance=1e-6):
             raise Exception(f'ELBO has not converged to within {tolerance} after {N} iterations')
     return (N,phi,m,s)
 
-if __name__=='__main__':
-    plt.rcParams.update({
-        "text.usetex": True
-    })         
-    
-    random.seed(1)
-    sigma = 1
-    k     = 3
-    mu    = sorted(np.random.normal(scale=sigma,size=k))
-    cs,xs = create_data(mu,sigma=sigma,n=1000)
+def plot_data(xs,cs,mu=[],sigmas=[],colours=['r','g','b', 'c', 'm', 'y']):
     plt.figure(figsize=(10,10))
     plt.hist(xs,
-             bins=25,
-             label='Full Dataset',
-             alpha=0.5)
-    x_values = np.arange(min(xs), max(xs), 0.1)
+             bins  = 25,
+             label = 'Full Dataset',
+             alpha = 0.5)
+    
     for i in range(len(mu)):
-        colour = ['r','g','b'][i]
-        x0s = [xs[j] for j in range(len(xs)) if cs[j]==i ]
+        colour   = colours[i]
+        x0s      = [xs[j] for j in range(len(xs)) if cs[j]==i ]
         n,bins,_ = plt.hist(x0s,
-                            bins=25,
-                            label=f'Component {i}',
-                            alpha=0.5,
-                            facecolor=colour)
-        y_values = stats.norm(mu[i], sigma)
-        plt.plot(x_values, [y*max(n)*2 for y in y_values.pdf(x_values)],
-                 c=colour,
-                 label=fr'$\mu=${mu[i]:.3f}, $\sigma=${sigma:.1f}')
+                            bins      = 25,
+                            label     = fr'$\mu=${mu[i]:.3f}, $\sigma=${sigmas[i]:.1f}',
+                            alpha     = 0.5,
+                            facecolor = colour)
+        x_values = np.arange(min(xs), max(xs), 0.1)
+        y_values = stats.norm(mu[i], sigmas[i])
+        plt.plot(x_values,
+                 [y*max(n)*2 for y in y_values.pdf(x_values)],
+                 c = colour)
+
     plt.legend()
     plt.title('Raw data')
     plt.xlabel('X')
     plt.ylabel('N')
     plt.savefig(os.path.basename(__file__).split('.')[0] )
     
-    iteration,phi,m,s = cavi(xs,k=k,N=100,sigma=sigma)
+if __name__=='__main__':
+    plt.rcParams.update({
+        "text.usetex": True
+    })         
+    parser = argparse.ArgumentParser('CAVI')
+    parser.add_argument('--K', type=int, default=3, help='Number of Gaussians')
+    parser.add_argument('--n', type=int, default=1000, help='Number of points')
+    args = parser.parse_args()
+    
+    random.seed(1)
+    
+    sigma  = 3
+    sigmas = [0.5]*args.K
+    mu     = sorted(np.random.normal(scale=sigma,size=args.K))
+    cs,xs  = create_data(mu,sigmas=sigmas,n=args.n)
+
+    plot_data(xs,cs,mu=mu,sigmas=sigmas)
+    iteration,phi,m,s = cavi(xs,K=args.K,N=100,sigmas=sigmas)
     print (phi)
     print (m)
     print (s)
