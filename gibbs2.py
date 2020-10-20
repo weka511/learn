19fa99b021ef14325bc2bf1910e6fc0261c2a4b1
Simon Crase <simon@greenweaves.nz>
2020-10-21 09:16:42 +1300

    Save plot files

--- gibbs2.py
+++ gibbs2.py
@@ -1,8 +1,6 @@
-from scipy.stats import uniform, gamma, poisson
-import matplotlib.pyplot as plt
 import numpy as np
-from numpy import log,exp
-from numpy.random import multinomial
+
+
 
 def gibbs(x,
           E         = 5200,
@@ -13,38 +11,44 @@ def gibbs(x,
     
     gibbs_sample = init()
     m            = len(gibbs_sample)
-    chain        = np.zeros((E-BURN_IN,n))
+    chain        = np.zeros((E-BURN_IN,m))
     
     for e in range(E):
         if e%frequency==0:
             print (f'At iteration {e}')
         for i in range(m):
-            gibbs_sample[i]  = sample(i,gibbs_sample)
+            gibbs_sample[i]    = sample(i,gibbs_sample)
         if e>=BURN_IN:
-            chain[e-BURN_IN] = gibbs_sample
+            chain[e-BURN_IN,:] = gibbs_sample
             
     return chain
 
 if __name__=='__main__':
-    N = 50
-    a = 2
-    b = 1
+    from scipy.stats import uniform, gamma, poisson
+    import matplotlib.pyplot as plt
     
-    n = int(round(uniform.rvs()*N)) # Change-point: where the intensity parameter changes.
-    print (f'Change point={n}')
+    from numpy import log,exp
+    from numpy.random import multinomial
+    import os
+    
+    N              = 50
+    a              = 2
+    b              = 1
+    
+    n              = int(round(uniform.rvs()*N)) # Change-point: where the intensity parameter changes.
     lambda1        = gamma.rvs(a,scale=1./b)
     lambda2        = gamma.rvs(a,scale=1./b)
     
     lambdas        = [lambda1]*n
     lambdas[n:N-1] = [lambda2]*(N-n)
-    
+
+    print (f'Change point={n}, lambdas=({lambda1},{lambda2})')    
     def init():
         return[int(round(uniform.rvs()*N)),
                gamma.rvs(a,scale=1./b),
                gamma.rvs(a,scale=1./b)]
     
     def sample(i,gibbs_sample):
-        assert i in [0,1,2]
 
         if i==0:
             n = int(gibbs_sample[2])
@@ -52,7 +56,7 @@ if __name__=='__main__':
         elif i==1:
             n = int(gibbs_sample[2])
             return gamma.rvs(a+sum(x[n:N]), scale=1./(N-n+b))
-        else:
+        elif i==2:
             lambda1 = gibbs_sample[0]
             lambda2 = gibbs_sample[1]            
             mult_n = np.array([0]*N)
@@ -62,20 +66,18 @@ if __name__=='__main__':
             n      = np.where(multinomial(1,mult_n/sum(mult_n),size=1)==1)[1][0]
             return n
  
-    x = poisson.rvs(lambdas)    
-   
-    
-    f, (ax1,ax2,ax3,ax4,ax5) = plt.subplots(5,1,figsize=(6,10))
-    f.tight_layout(pad=1.0)
-    # Plot the data
-    ax1.stem(range(N),x,linefmt='b-', markerfmt='bo')
-    ax1.plot(range(N),lambdas,'r--')
-    ax1.set_ylabel('Counts')
-    
-    chain = gibbs(x,init = init, sample= sample)
+    x             = poisson.rvs(lambdas)    
+    chain         = gibbs(x,init = init, sample= sample)
     chain_lambda1 = chain[:,0]
     chain_lambda2 = chain[:,1]
     chain_n       = chain[:,2]
+    
+    f, (ax1,ax2,ax3,ax4,ax5) = plt.subplots(5,1,figsize=(6,10))
+    f.tight_layout(pad=1.0)
+
+    ax1.stem(range(N),x,linefmt='b-', markerfmt='bo')
+    ax1.plot(range(N),lambdas,'r--')
+    ax1.set_ylabel('Counts')
     
     ax2.plot(chain_lambda1,'b',chain_lambda2,'g')
     ax2.set_ylabel('$\lambda$')
@@ -91,5 +93,7 @@ if __name__=='__main__':
     ax5.hist(chain_n,50)
     ax5.set_title('n')
     ax5.set_xlim([1,50])
+    
+    plt.savefig(os.path.basename(__file__).split('.')[0] )
     
     plt.show()    
