e007d56c3589a786d6ba98f90c5f00e36ee6a5d1
Simon Crase <simon@greenweaves.nz>
2022-10-06 10:04:19 +1300

    #43 Tidied up plots

--- feex5.py
+++ feex5.py
@@ -27,45 +27,64 @@ from random            import random
 
 rc('text', usetex=True)
 
-XKCD = ['xkcd:purple','xkcd:green','xkcd:blue','xkcd:pink',
-        'xkcd:brown','xkcd:red','xkcd:light blue',
-        'xkcd:teal','xkcd:orange','xkcd:light green','xkcd:magenta','xkcd:yellow',]
+class Colours:
+    def __init__(self):
+        self.XKCD = ['xkcd:purple','xkcd:green','xkcd:blue','xkcd:pink',
+                'xkcd:brown','xkcd:red','xkcd:light blue',
+                'xkcd:teal','xkcd:orange','xkcd:light green','xkcd:magenta','xkcd:yellow']
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        value = self.XKCD[self.i]
+        self.i += 1
+        return value
+
 phi_mean  = 5
 phi_sigma = 2
 phi_above = 5
 
-dt = 0.01
-MaxT = 20
-N = 1000
+dt    = 0.01
+MaxT  = 20
+N     = 1000
 LRate = 0.01
 
 Sigma = [1]
 
 fig = figure(figsize=(10,10))
 ax  = fig.add_subplot(2,1,1)
-k=0
+colors = Colours()
+cc     = iter(colors)
+
 for i in range(N):
     error = [1]
     e     = [0]
     phi = phi_mean + sqrt(phi_sigma) * random()
     for j in range(int(MaxT/dt)):
-        error1 = error[-1]+dt*(phi-phi_above - e[-1])
-        e1     = e[-1] + dt *(Sigma[-1] * error[-1] - e[-1])
-        error.append(error1)
-        e.append(e1)
+        error.append(error[-1]+dt*(phi-phi_above - e[-1]))
+        e.append(e[-1] + dt *(Sigma[-1] * error[-2] - e[-1]))
+
     Sigma.append(Sigma[-1] + LRate *(error[-1]*error[-1]-1))
     if i%100==0:
-        ax.plot(error, linestyle='dotted', c=XKCD[k],label=f'error {i}')
-        ax.plot(e, linestyle='dashed', c=XKCD[k])
-        k+= 1
-
+        c = next(colors)
+        ax.plot(error,
+                linestyle = 'dotted',
+                c         = c,
+                label     = 'error' if i==0 else None)
+        ax.plot(e,
+                linestyle = 'dashed',
+                c         = c,
+                label     ='e' if i==0 else None)
+ax.set_title('Errors')
 ax.legend()
 
 ax  = fig.add_subplot(2,1,2)
-
-
 ax.plot(Sigma)
 ax.set_xlabel('Trial')
 ax.set_ylabel(r'$\Sigma$')
+ax.set_title(r'Evolution of $\Sigma$')
+fig.suptitle('Exercise 5')
 fig.savefig('feex5')
 show()
