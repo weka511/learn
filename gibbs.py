3bfa0740654eb56e40f37eaa9bf2c21fd34758ba
Simon Crase <simon@greenweaves.nz>
2025-11-25 11:06:06 +1300

    Closed #45

--- gibbs.py
+++ gibbs.py
@@ -26,11 +26,13 @@ from argparse import ArgumentParser
 from os.path import basename, join
 from time import time
 from matplotlib.pyplot import figure, show
+from matplotlib import rc
 import numpy as np
 from numpy.random import default_rng
 from pymdp.maths import softmax
 
-def generate(rng, N = 50, a = 2, b = 1, K = 5):
+
+def generate(rng, N=50, a=2, b=1, K=5):
     '''
     Generate a sequence of counts. The first n counts follow a Poisson distribution with mean lambda1, and the remainder
     follow Poisson with mean lambda2. The two lambda are sampled from a Gamma distribution, and n is also random.
@@ -47,23 +49,23 @@ def generate(rng, N = 50, a = 2, b = 1, K = 5):
     '''
     for k in range(K):
         try:
-            n = int(round(rng.uniform()*N)) # Change-point: where the intensity parameter changes.
-            lambda1 = rng.gamma(a,scale=1./b) # Intensity value
-            lambda2 = rng.gamma(a,scale=1./b) # Intensity value
+            n = int(round(rng.uniform() * N)) # Change-point: where the intensity parameter changes.
+            lambda1 = rng.gamma(a, scale=1. / b) # Intensity value
+            lambda2 = rng.gamma(a, scale=1. / b) # Intensity value
 
             lambdas = np.empty((N))
             lambdas[0:n] = lambda1
-            lambdas[n:N-1] = lambda2
+            lambdas[n:N - 1] = lambda2
 
             x = rng.poisson(lambdas)     # Observations, x_1 ... x_N
 
             return x, lambdas
         except ValueError as value_error:
-            print (f'Failed to generate data\n{value_error}')
+            print(f'Failed to generate data\n{value_error}')
             exit(1)
 
 
-def gibbs(rng, x, E  = 5200, BURN_IN = 200, frequency = 100, a = 2, b = 1, report = lambda epoch: print (f'Epoch={epoch}')):
+def sample(rng, x, E=5200, BURN_IN=200, freq=100, a=2, b=1, report=lambda epoch: print(f'Epoch={epoch}')):
     '''
     Gibbs sampler. We simulate samples by sweeping through all the posterior conditionals,
     one random variable at a time.
@@ -73,10 +75,10 @@ def gibbs(rng, x, E  = 5200, BURN_IN = 200, frequency = 100, a = 2, b = 1, repor
         x          Sequence of counts for analysis
         E          Number of epochs for sampling
         BURN_IN    Burn in period (number of epochs that will not be recorded)
-        frequency  Frequencey for reporting
+        freq       Frequency for reporting
+        report     Reporter function called every freq epochs
         a          loc parameter for gamma distribution
         b          1/scale parameter for gamma distribution
-        report     Reporter function called every frequency epochs
 
     Returns:
         chain_lambda1
@@ -90,85 +92,91 @@ def gibbs(rng, x, E  = 5200, BURN_IN = 200, frequency = 100, a = 2, b = 1, repor
     chain_lambda2 = np.zeros(E - BURN_IN)
 
     for epoch in range(E):
-        if epoch%frequency == 0: report(epoch)
-        lambda1 = rng.gamma(a+sum(x[0:n]), scale=1./(n+b))  # sample lambda1 from its  posterior conditional,Equation 8
-        lambda2 = rng.gamma(a+sum(x[n:N]), scale=1./(N-n+b)) # sample ambda2 from its posterior conditional, Equation 9
+        if epoch % freq == 0: report(epoch)
+        lambda1 = rng.gamma(a + sum(x[0:n]), scale=1. / (n + b))  # sample lambda1 from its  posterior conditional,Equation 8
+        lambda2 = rng.gamma(a + sum(x[n:N]), scale=1. / (N - n + b)) # sample ambda2 from its posterior conditional, Equation 9
         mult_n = np.zeros(N)          # Now sample n
         for i in range(N): # Equation 10
-            mult_n[i] = sum(x[0:i])*np.log(lambda1) - i*lambda1 + sum(x[i:N])*np.log(lambda2) - (N-i)*lambda2
+            mult_n[i] = sum(x[0:i]) * np.log(lambda1) - i * lambda1 + sum(x[i:N]) * np.log(lambda2) - (N - i) * lambda2
 
-        n = np.nonzero(rng.multinomial(1,softmax(mult_n),size=1))[1][0]
+        n = np.nonzero(rng.multinomial(1, softmax(mult_n), size=1))[1][0]
         if epoch < BURN_IN: continue
         chain_n[epoch - BURN_IN] = n
         chain_lambda1[epoch - BURN_IN] = lambda1
         chain_lambda2[epoch - BURN_IN] = lambda2
 
-    return (chain_lambda1,chain_lambda2,chain_n)
+    return (chain_lambda1, chain_lambda2, chain_n)
+
 
 def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--seed', type=int, help='Seed for random number generator')
-    parser.add_argument('--E',type=int,default = 5200, help='Number of Epochs')
-    parser.add_argument('--BURN_IN', type=int, default = 200, help='Burn in: number of Epochs to skip at begining')
-    parser.add_argument('--frequency', type=int, default = 1000, help='For recording progress')
-    parser.add_argument('--a', type=float, default = 2, help='Paramater for lambda1')
-    parser.add_argument('--b', type=float,default = 1, help='Paramater for lambda2')
-    parser.add_argument('--N', type=int, default = 50, help='Number of data points to generate')
-    parser.add_argument('--m', type=int, default = 5, help='Number of Mont Carlo runs')
+    parser.add_argument('--E', type=int, default=5200, help='Number of Epochs')
+    parser.add_argument('--BURN_IN', type=int, default=200, help='Burn in: number of Epochs to skip at begining')
+    parser.add_argument('--freq', type=int, default=1000, help='For recording progress')
+    parser.add_argument('--a', type=float, default=2, help='Paramater for lambda1')
+    parser.add_argument('--b', type=float, default=1, help='Paramater for lambda2')
+    parser.add_argument('--N', type=int, default=50, help='Number of data points to generate')
+    parser.add_argument('--m', type=int, default=5, help='Number of Mont Carlo runs')
     parser.add_argument('--figs', default='./figs', help='Folder to store plots')
-    parser.add_argument('--show', default = False, action = 'store_true', help='Controls whether plot will be shown')
+    parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be shown')
     return parser.parse_args()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
+    rc('font', **{'family': 'serif',
+                  'serif': ['Palatino'],
+                  'size': 8})
+    rc('text', usetex=True)
     start = time()
     args = parse_args()
     rng = np.random.default_rng(args.seed)
-    x, lambdas  = generate(rng,N = args.N, a = args.a, b = args.b)
-    fig = figure(figsize=(14,14))
+    x, lambdas = generate(rng, N=args.N, a=args.a, b=args.b)
+    fig = figure(figsize=(14, 14))
     fig.tight_layout(pad=3.0)
     fig.suptitle('Gibbs sampler for a change-point model')
 
-    ax1 = fig.add_subplot(args.m,4,1)
-    ax1.stem(range(len(x)),x,linefmt='b-', markerfmt='bo',label='Counts')
-    ax1.plot(range(len(x)),lambdas,'r--',label=r'$\lambda$')
+    ax1 = fig.add_subplot(args.m, 4, 1)
+    ax1.stem(range(len(x)), x, linefmt='b-', markerfmt='bo', label='Counts')
+    ax1.plot(range(len(x)), lambdas, 'r--', label=r'$\lambda$')
     ax1.set_ylabel('Counts')
     ax1.set_title('Observations')
     ax1.legend()
 
     for i in range(args.m):
-        chain_lambda1,chain_lambda2,chain_n = gibbs(rng,x, E=args.E, BURN_IN=args.BURN_IN, frequency=args.frequency,
-                                                    a = args.a, b = args.b,
-                                                    report = lambda epoch: print (f'Run={i},Epoch={epoch}'))
+        chain_lambda1, chain_lambda2, chain_n = sample(rng, x, E=args.E, BURN_IN=args.BURN_IN, a=args.a, b=args.b,
+                                                       freq=args.freq, report=lambda epoch: print(f'Run={i},Epoch={epoch}'))
 
-        ax2 = fig.add_subplot(args.m,4,4*i+2)
-        ax2.plot(chain_lambda1,'b',label=r'$\lambda 1$')
-        ax2.plot(chain_lambda2,'g',label=r'$\lambda 2$')
-        if i== args.m - 1:
+        ax2 = fig.add_subplot(args.m, 4, 4 * i + 2)
+        ax2.plot(chain_lambda1, 'b', label=r'$\lambda 1$')
+        ax2.plot(chain_lambda2, 'g', label=r'$\lambda 2$')
+        if i == args.m - 1:
             ax2.set_xlabel(r'$Epoch$')
         ax2.set_ylabel(r'$\lambda$')
+        ax2.set_title(r'$\lambda$')
         ax2.legend()
 
-        ax3 = fig.add_subplot(args.m,4,4*i+3)
-        ax3.hist(chain_lambda2,20,color='g',label=r'$\lambda 2$',alpha=0.5)
-        ax3.hist(chain_lambda1,20,color='b',label=r'$\lambda 1$',alpha=0.5)
-        ax3.set_xlim([0,12])
-        if i==0:
+        ax3 = fig.add_subplot(args.m, 4, 4 * i + 3)
+        ax3.hist(chain_lambda2, 20, color='g', label=r'$\lambda 2$', alpha=0.5)
+        ax3.hist(chain_lambda1, 20, color='b', label=r'$\lambda 1$', alpha=0.5)
+        ax3.set_xlim([0, 12])
+        if i == 0:
             ax3.set_title(r'$\lambda$')
         ax3.legend()
 
-        ax4 = fig.add_subplot(args.m,4,4*i+4)
-        ax4.hist(chain_n,args.N,label='n')
-        if i==0:
+        ax4 = fig.add_subplot(args.m, 4, 4 * i + 4)
+        ax4.hist(chain_n, args.N, label='n')
+        if i == 0:
             ax4.set_title('Histogram for Change Point')
-        ax4.set_xlim([0,args.N])
+        ax4.set_xlim([0, args.N])
         ax4.legend()
 
-    fig.savefig(join(args.figs,f'{basename(__file__).split('.')[0]}') )
+    fig.savefig(join(args.figs, f'{basename(__file__).split('.')[0]}'))
 
     elapsed = time() - start
-    minutes = int(elapsed/60)
-    seconds = elapsed - 60*minutes
-    print (f'Elapsed Time {minutes}m {seconds:.2f}s')
+    minutes = int(elapsed / 60)
+    seconds = elapsed - 60 * minutes
+    print(f'Elapsed Time {minutes}m {seconds:.2f}s')
 
     if args.show:
         show()
