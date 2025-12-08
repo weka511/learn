28643644019f73c9facff9333632b953efb2cc5a
Simon Crase <simon@greenweaves.nz>
2025-12-08 21:12:57 +1300

    #55 Use xkcd colours

--- xkcd.py
+++ xkcd.py
@@ -21,24 +21,28 @@
 
 from re import split
 
-def generate_xkcd_colours(file_name = 'rgb.txt', prefix = 'xkcd:', filter = lambda R,G,B:True):
-    '''  Create list of XKCD colours
-         Keyword Parameters:
+
+def generate_xkcd_colours(file_name='rgb.txt', prefix='xkcd:', filter=lambda R, G, B: True):
+    '''
+        Create list of XKCD colours
+
+        Keyword Parameters:
             file_name Where XKCD colours live
             prefix    Use to prefix each colour with "xkcd:"
             filter    Allows us to exclude some colours based on RGB values
     '''
     with open(file_name) as colours:
         for row in colours:
-            parts = split(r'\s+#',row.strip())
-            if len(parts)>1:
-                rgb = int(parts[1],16)
-                B = rgb%256
-                rest = (rgb-B)//256
-                G = rest%256
-                R = (rest-G)//256
-                if filter(R,G,B):
+            parts = split(r'\s+#', row.strip())
+            if len(parts) > 1:
+                rgb = int(parts[1], 16)
+                B = rgb % 256
+                rest = (rgb - B) // 256
+                G = rest % 256
+                R = (rest - G) // 256
+                if filter(R, G, B):
                     yield f'{prefix}{parts[0]}'
 
-def create_xkcd_colours(file_name = 'rgb.txt', prefix = 'xkcd:', filter = lambda R,G,B:True):
-    return [colour for colour in generate_xkcd_colours(file_name=file_name,prefix=prefix,filter = filter)][::-1]
+
+def create_xkcd_colours(file_name='rgb.txt', prefix='xkcd:', filter=lambda R, G, B: True):
+    return [colour for colour in generate_xkcd_colours(file_name=file_name, prefix=prefix, filter=filter)][::-1]
