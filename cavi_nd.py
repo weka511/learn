a2985dea201d42c93a65b6bf63743daf2d7fd9d2
Simon Crase <simon@greenweaves.nz>
2025-12-20 09:56:31 +1300

    #40 Clustered sucessfully in 2D for K=3 and 5

--- cavi_nd.py
+++ cavi_nd.py
@@ -28,106 +28,147 @@ from os.path import basename, join
 from matplotlib.pyplot import figure, rcParams, show
 import numpy as np
 from xkcd import generate_xkcd_colours
-from gmm import GaussionMixtureModel, get_name, create_colours,generate_xkcd_colours
+from gmm import GaussionMixtureModel, get_name, create_colours
+
 
 class Solution:
-    def __init__(self):
-        self.ELBO = []
+    '''
+    This class represents the results of one run of CAVI.
 
-    def set_params(self,m,s,c):
+    Parameters:
+        ELBO
+        m
+        s
+        c
+    '''
+    def __init__(self):
+        '''
+        Used at the start of a run to initalize parameters
+        '''
+        self.ELBO = []
+        self.m = None
+        self.s = None
+        self.c = None
+
+    def set_params(self, m, s, c):
+        '''
+        Used at the end of a run to store results
+        '''
         self.m = m.copy()
         self.s = s.copy()
         self.c = c.copy()
 
-    def append_ELBO(self,ELBO):
+    def append_ELBO(self, ELBO):
+        '''
+        Used during a run to accumulate ELBO for each iteration
+        '''
         self.ELBO.append(ELBO)
+
 
 def parse_args():
     parser = ArgumentParser(__doc__)
-    parser.add_argument('--name')
+    parser.add_argument('name',help='Name of data file')
     parser.add_argument('--seed', type=int, default=None, help='Seed for random number generator')
     parser.add_argument('--K', type=int, default=3, help='Number of Gaussians')
     parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot displayed')
     parser.add_argument('--N', type=int, default=100, help='Number of iterations per run')
     parser.add_argument('--M', type=int, default=25, help='Number of runs')
-    parser.add_argument('--BURN_IN',type=int, default=3,help='Minimum number of iterations')
+    parser.add_argument('--BURN_IN', type=int, default=3, help='Minimum number of iterations')
     parser.add_argument('--atol', type=float, default=1e-6, help='Tolerance for improving ELBO')
     parser.add_argument('--sigma', type=float, default=1, help='Standard deviation')
     parser.add_argument('--figs', default='./figs', help='Folder to store plots')
     parser.add_argument('--path', default='./data', help='Path to folder where data are stored')
     return parser.parse_args()
 
-def initialize(x,K, rng = np.random.default_rng()):
-    n,d = x.shape
-    s = np.ones((K,d))
-    indices = rng.integers(0,n,size=K)
-    mu0 = np.empty((K,d))
+
+def initialize(x, K, rng=np.random.default_rng()):
+    n, d = x.shape
+    s = np.ones((K, d))
+    indices = rng.integers(0, n, size=K)
+    mu0 = np.empty((K, d))
     for k in range(K):
-        mu0[k,:] = x[indices[k],:]
+        mu0[k, :] = x[indices[k], :]
 
-    c = np.zeros((n,K))
+    c = np.zeros((n, K))
     for i in range(n):
-        c[i, rng.integers(0,2)] = 1
+        c[i, rng.integers(0, 2)] = 1
 
-    return mu0,s,c
+    return mu0, s, c
 
-def get_ELBO(m,s,c,x):
+
+def get_ELBO(m, s, c, x):
     '''
     Calculate ELBO using equation (18) of my notes
     '''
     def create_offsets():
-        n,d = x.shape
-        _,K = c.shape
-        offsets = np.zeros((n,K))
+        n, d = x.shape
+        _, K = c.shape
+        offsets = np.zeros((n, K))
         for i in range(n):
             for k in range(K):
                 for j in range(d):
-                    offsets[i,k] += s[k,j]**2 + (x[i,j] - m[k,j])**2
+                    offsets[i, k] += s[k, j]**2 + (x[i, j] - m[k, j])**2
         return offsets
-    return -0.5 * ( np.sum(m**2 + s**2) + np.sum(c * create_offsets()))
+    return -0.5 * (np.sum(m**2 + s**2) + np.sum(c * create_offsets()))
 
-def get_updated_assignments(m,s,x):
-    n,d = x.shape
-    K,_ = m.shape
-    log_c = np.zeros((n,K))
+
+def get_updated_assignments(m, s, x):
+    n, d = x.shape
+    K, _ = m.shape
+    log_c = np.zeros((n, K))
     for i in range(n):
         for k in range(K):
             for j in range(d):
-                log_c[i,k] += m[k,j]*x[i,j] - 0.5 *(s[k,j]**2 + m[k,j]**2)
+                log_c[i, k] += m[k, j] * x[i, j] - 0.5 * (s[k, j]**2 + m[k, j]**2)
     c = np.exp(log_c)
-    Z = np.sum(c,axis=1)
+    Z = np.sum(c, axis=1)
     for i in range(n):
-        c[i,:] /= Z[i]
+        c[i, :] /= Z[i]
     return c
 
-def get_updated_statistics(m0,s0,c,x,sigma=1):
-    n,d = x.shape
-    K,_ = m0.shape
-    denominator = 1/sigma**2 * np.ones((K))
+
+def get_updated_statistics(m0, s0, c, x, sigma=1):
+    n, d = x.shape
+    K, _ = m0.shape
+    denominator = 1 / sigma**2 * np.ones((K))
     for i in range(n):
         for k in range(K):
-            denominator[k] += c[i,k]
+            denominator[k] += c[i, k]
     m = np.zeros_like(m0)
     for i in range(n):
         for k in range(K):
             for j in range(d):
-                m[k,j] += c[i,k] * x[i,j]
+                m[k, j] += c[i, k] * x[i, j]
     for j in range(d):
-        m[:,j] /= denominator
+        m[:, j] /= denominator
     s = np.empty_like(s0)
     for j in range(d):
-        s[:,j] = np.sqrt(1/denominator)
-    return m,s
+        s[:, j] = np.sqrt(1 / denominator)
+    return m, s
 
-def display(l,x,c,fig,rows=10,cols=10):
-    ax = fig.add_subplot(rows,cols,l)
-    n,d = x.shape
-    x_colours = np.empty((n),dtype=np.dtypes.StringDType())
+
+def display(l, x, c, fig, rows=10, cols=10):
+    ax = fig.add_subplot(rows, cols, l)
+    n, d = x.shape
+    x_colours = np.empty((n), dtype=np.dtypes.StringDType())
     for i in range(n):
-        index = np.argmax(c[i,:])
+        index = np.argmax(c[i, :])
         x_colours[i] = colours[index]
-    ax.scatter(x[:,0],x[:,1],c=x_colours,s=1)
-    return rows*cols
+    ax.scatter(x[:, 0], x[:, 1], c=x_colours, s=1)
+    return rows * cols
+
+
+def create_data_colours(x, c):
+    '''
+    Create a set of colours for use when we display points in their clusters
+    '''
+    n, d = x.shape
+    colours = np.empty((n), dtype=np.dtypes.StringDType())
+    for i in range(n):
+        index = np.argmax(c[i, :])
+        colours[i] = cluster_colours[index]
+    return colours
+
 
 if __name__ == '__main__':
     rcParams.update({
@@ -137,8 +178,6 @@ if __name__ == '__main__':
     args = parse_args()
     rng = np.random.default_rng(args.seed)
 
-    fig = figure(figsize=(12, 12))
-
     cluster_colours = create_colours(args.K)
     ELBO_colours = generate_xkcd_colours()
     model = GaussionMixtureModel(name=get_name(args))
@@ -147,35 +186,47 @@ if __name__ == '__main__':
     index_best = -1
 
     for i in range(args.M):
-        print (i)
-        m,s,c = initialize(x,args.K,rng=rng)
+        print(f'{i+1}/{args.M}')
+        m, s, c = initialize(x, args.K, rng=rng)
         Solutions.append(Solution())
-        Solutions[-1].append_ELBO( get_ELBO(m,s,c,x))
+        Solutions[-1].append_ELBO(get_ELBO(m, s, c, x))
 
         for j in range(args.N):
-            c = get_updated_assignments(m,s,x)
-            m,s = get_updated_statistics(m,s,c,x)
-            Solutions[-1].append_ELBO(get_ELBO(m,s,c,x))
-            if len(Solutions) > args.BURN_IN and Solutions[-1].ELBO[-1] - Solutions[-1].ELBO[-2] < args.atol: break
+            c = get_updated_assignments(m, s, x)
+            m, s = get_updated_statistics(m, s, c, x)
+            Solutions[-1].append_ELBO(get_ELBO(m, s, c, x))
+            if len(Solutions) > args.BURN_IN and Solutions[-1].ELBO[-1] - Solutions[-1].ELBO[-2] < args.atol:
+                break
 
-        Solutions[-1].set_params(m,s,c)
-        if index_best == -1 or Solutions[-1].ELBO[-1] < Solutions[index_best].ELBO[-1]:
+        Solutions[-1].set_params(m, s, c)
+        if index_best == -1 or Solutions[-1].ELBO[-1] > Solutions[index_best].ELBO[-1]:
             index_best = i
 
-    ax1 = fig.add_subplot(2,1,1)
+    fig = figure(figsize=(12, 12))
+    fig.suptitle(f'{args.name}')
+    ax1 = fig.add_subplot(2, 1, 1)
     for i in range(args.M):
-        ax1.plot(Solutions[i].ELBO,c=next(ELBO_colours),label =f'best {Solutions[i].ELBO[-1]:.6}' if i==index_best else None)
+        label = None
+        linestyle = 'dotted'
+        if i == index_best:
+            label = f'best {Solutions[i].ELBO[-1]:.6}'
+            linestyle = 'solid'
+        ax1.plot(range(len(Solutions[i].ELBO)),Solutions[i].ELBO, c=next(ELBO_colours), label=label, linestyle=linestyle)
     ax1.legend()
+    ax1.set_title(f'ELBO for {args.M} runs')
+    ax1.set_xlabel('Iteration')
+    ax1.set_ylabel('ELBO')
+    ax2 = fig.add_subplot(2, 1, 2)
 
-    ax2 = fig.add_subplot(2,1,2)
-    n,d = x.shape
-    x_colours = np.empty((n),dtype=np.dtypes.StringDType())
-    for i in range(n):
-        index = np.argmax(c[i,:])
-        x_colours[i] = cluster_colours[index]
-    ax2.scatter(x[:,0],x[:,1],c=x_colours,s=1)
+    ax2.scatter(x[:, 0], x[:, 1], c=create_data_colours(x, Solutions[index_best].c), s=1)
     for k in range(args.K):
-        ax2.scatter(Solutions[index_best].m[k,0],Solutions[index_best].m[k,1],c=cluster_colours[k])
+        ax2.scatter(Solutions[index_best].m[k, 0], Solutions[index_best].m[k, 1], c='xkcd:black', marker='+', s=25)
+    ax2.set_title(f'Solution with best ELBO {Solutions[index_best].ELBO[-1]:.6} after {args.M} runs')
+    ax2.set_xlabel('X')
+    ax2.set_ylabel('Y')
+
+    fig.tight_layout(pad=3,h_pad=4)
+    fig.savefig(join(args.figs, f'{basename(__file__).split('.')[0]}'))
 
     if args.show:
         show()
