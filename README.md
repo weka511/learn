ec5d13fdfd6436a2106cfdc7ec26237a1ae117ff
Simon Crase <simon@greenweaves.nz>
2025-12-25 09:25:38 +1300

    Closed #75

--- README.md
+++ README.md
@@ -1,21 +1,16 @@
 # learn
 Experiments in machine learning
 
-## General
+## Neural Networks
 
  File  | Remarks
 ---------------|--------------------------------------------
-bgr.txt|List of xkcd colours, organized by most widely recognized first
 chainsaw.py|Cut logs up and process them
 gibbs.py |  [Bayesian Inference: Gibbs Sampling](http://www2.bcs.rochester.edu/sites/jacobslab/cheat_sheets.html)
 hopfield.py | Hopfield network
 mhast.py | [Bayesian Inference: Metropolis-Hastings](http://www2.bcs.rochester.edu/sites/jacobslab/cheat_sheets.html)
-learn.bib|Bibliography
-learn.wpr | Project file for Wing IDE
 mnist.py|Testbed for deep neural networks, using ideas from Goodfellow et al. Learn to recognize images from the NIST dataset
 torch-nn.py|[train](https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#sphx-glr-beginner-blitz-cifar10-tutorial-py)
-UGMM.py|CAVI code snarfed from [Zhiya Zuo](https://zhiyzuo.github.io/VI)
-xkcd.py|Build lists of [XKCD colours](https://xkcd.com/color/rgb/)
 
 ## Variational Inference
 
@@ -29,6 +24,16 @@ CAVI.tex|Doco for VI programs
 cavi1.py|CAVI for Univariate Gaussian from [Univariate Gaussian Example](https://suzyahyah.github.io/bayesian%20inference/machine%20learning/2019/03/20/CAVI.html)
 cavi3.py|The Coordinate Ascent Mean-Field Variational Inference (CAVI) example from Section 3 of Blei et al
 cavi.py|The Coordinate Ascent Mean-Field Variational Inference (CAVI) example from Section 3 of Blei et al
-cavr_nd.py|A version of cavi.py that works in multiple dimensions
+cavi_nd.py|A version of cavi.py that works in multiple dimensions
 em.py|Expectation Maximization
-gmm.py|Generate data in accordance with Gaussian Mixture Model
+gmm.py|Generate data in accordance with Gaussian Mixture Model for cavi_nd.py
+UGMM.py|CAVI code snarfed from [Zhiya Zuo](https://zhiyzuo.github.io/VI)
+
+## supporting code
+
+ File  | Remarks
+---------------|--------------------------------------------
+bgr.txt|List of [XKCD colours](https://xkcd.com/color/rgb/), in reverse order, so they are organized by most widely recognized first
+learn.bib|Bibliography
+learn.wpr | Project file for Wing IDE
+xkcd.py|Used to supply [XKCD colours](https://xkcd.com/color/rgb/)
--- UGMM.py
+++ UGMM.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-# snarfed from https://zhiyzuo.github.io/VI/#python-implementation
+'''
+    CAVI Gaussian Mixture Model
+    snarfed from https://zhiyzuo.github.io/VI/#python-implementation
+'''
 
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure, show
 import numpy as np
 import seaborn as sns
 
 class UGMM(object):
     '''Univariate GMM with CAVI'''
+
     def __init__(self, X, K=2, sigma=1):
         self.X = X
         self.K = K
@@ -14,19 +18,17 @@ class UGMM(object):
         self.sigma2 = sigma**2
 
     def _init(self):
-        self.phi = np.random.dirichlet([np.random.random()*np.random.randint(1, 10)]*self.K, self.N)
+        self.phi = np.random.dirichlet([np.random.random() * np.random.randint(1, 10)] * self.K, self.N)
         self.m = np.random.randint(int(self.X.min()), high=int(self.X.max()), size=self.K).astype(float)
-        self.m += self.X.max()*np.random.random(self.K)
+        self.m += self.X.max() * np.random.random(self.K)
         self.s2 = np.ones(self.K) * np.random.random(self.K)
-        print('Init mean')
-        print(self.m)
-        print('Init s2')
-        print(self.s2)
+        print(f'Init mean {self.m}')
+        print(f'Init s2 {self.s2}')
 
     def get_elbo(self):
-        t1 = np.log(self.s2) - self.m/self.sigma2
+        t1 = np.log(self.s2) - self.m / self.sigma2
         t1 = t1.sum()
-        t2 = -0.5*np.add.outer(self.X**2, self.s2+self.m**2)
+        t2 = -0.5 * np.add.outer(self.X**2, self.s2 + self.m**2)
         t2 += np.outer(self.X, self.m)
         t2 -= np.log(self.phi)
         t2 *= self.phi
@@ -38,7 +40,7 @@ class UGMM(object):
         self.elbo_values = [self.get_elbo()]
         self.m_history = [self.m]
         self.s2_history = [self.s2]
-        for iter_ in range(1, max_iter+1):
+        for iter_ in range(1, max_iter + 1):
             self._cavi()
             self.m_history.append(self.m)
             self.s2_history.append(self.s2)
@@ -47,13 +49,12 @@ class UGMM(object):
                 print(iter_, self.m_history[iter_])
                 print(iter_, self.s2_history[iter_])
             if np.abs(self.elbo_values[-2] - self.elbo_values[-1]) <= tol:
-                print('ELBO converged with ll %.3f at iteration %d'%(self.elbo_values[-1],
-                                                                     iter_))
+                print('ELBO converged with ll %.3f at iteration %d' % (self.elbo_values[-1],
+                                                                       iter_))
                 break
 
         if iter_ == max_iter:
-            print('ELBO ended with ll %.3f'%(self.elbo_values[-1]))
-
+            print('ELBO ended with ll %.3f' % (self.elbo_values[-1]))
 
     def _cavi(self):
         self._update_phi()
@@ -61,37 +62,47 @@ class UGMM(object):
 
     def _update_phi(self):
         t1 = np.outer(self.X, self.m)
-        t2 = -(0.5*self.m**2 + 0.5*self.s2)
+        t2 = -(0.5 * self.m**2 + 0.5 * self.s2)
         exponent = t1 + t2[np.newaxis, :]
         self.phi = np.exp(exponent)
         self.phi = self.phi / self.phi.sum(1)[:, np.newaxis]
 
-
     def _update_mu(self):
-        self.m = (self.phi*self.X[:, np.newaxis]).sum(0) * (1/self.sigma2 + self.phi.sum(0))**(-1)
+        self.m = (self.phi * self.X[:, np.newaxis]).sum(0) * (1 / self.sigma2 + self.phi.sum(0))**(-1)
         assert self.m.size == self.K
-        self.s2 = (1/self.sigma2 + self.phi.sum(0))**(-1)
+        self.s2 = (1 / self.sigma2 + self.phi.sum(0))**(-1)
         assert self.s2.size == self.K
 
 
 num_components = 3
-mu_arr = np.random.choice(np.arange(-10, 10, 2),
-                          num_components) +   np.random.random(num_components)
-
-
 SAMPLE = 1000
+
+mu_arr = np.random.choice(np.arange(-10, 10, 2),
+                          num_components) + np.random.random(num_components)
 
 X = np.random.normal(loc=mu_arr[0], scale=1, size=SAMPLE)
 for i, mu in enumerate(mu_arr[1:]):
     X = np.append(X, np.random.normal(loc=mu, scale=1, size=SAMPLE))
-plt.figure(figsize=(15, 4))
-ax = plt.subplot(2,1,1)
-sns.distplot(X[:SAMPLE], ax=ax, rug=True)
-sns.distplot(X[SAMPLE:SAMPLE*2], ax=ax, rug=True)
-sns.distplot(X[SAMPLE*2:], ax=ax, rug=True)
+
+fig = figure(figsize=(15, 8))
+ax = fig.add_subplot(2, 1, 1)
+sns.histplot(X[:SAMPLE], ax=ax, kde=True, stat='density',label=f'First {SAMPLE} points')
+sns.rugplot(X[:SAMPLE])
+sns.histplot(X[SAMPLE:SAMPLE * 2], ax=ax, kde=True, stat='density',label=f'Middle {SAMPLE} points')
+sns.rugplot(X[SAMPLE:SAMPLE * 2])
+sns.histplot(X[SAMPLE * 2:], ax=ax, kde=True, stat='density',label=f'Last {SAMPLE} points')
+sns.rugplot(X[SAMPLE * 2:])
+
+ax.set_title('Data and fitted model')
+ax.legend()
 
 ugmm = UGMM(X, 3)
 ugmm.fit()
-ax1=plt.subplot(2,1,2)
+
+ax1 = fig.add_subplot(2, 1, 2)
 ax1.plot(ugmm.elbo_values)
-plt.show()
+ax1.set_ylabel('ELBO')
+ax1.set_title('Convergence')
+fig.tight_layout(pad=3, h_pad=9, w_pad=3)
+
+show()
