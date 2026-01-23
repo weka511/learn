1c37a81e13540905e65edfb515920814ec3a655a
Simon Crase <simon@greenweaves.nz>
2026-01-23 16:46:28 +1300

    #89 Plot learning rate

--- aetrain.py
+++ aetrain.py
@@ -74,6 +74,7 @@ class Perceptron(nn.Module):
         optimizer.step()
         optimizer.zero_grad()
 
+
 class OptimizerFactory:
     '''
     This class instantiates optimizers as specified by the command line parameters
@@ -96,10 +97,10 @@ class OptimizerFactory:
         match args.optimizer:
             case 'SGD':
                 text = (f'optimizer=SGD, lr={args.lr}, weight_decay={args.weight_decay},momentum={args.momentum}')
-                return SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay,momentum=args.momentum),text
+                return SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum), text
             case 'Adam':
                 text = f'optimizer=Adam, lr={args.lr}, weight_decay={args.weight_decay}'
-                return Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay),text
+                return Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay), text
 
 
 def parse_args(factory):
@@ -114,7 +115,10 @@ def parse_args(factory):
     training_group.add_argument('--N', default=5, type=int, help='Number of epochs')
     training_group.add_argument('--n', default=5, type=int, help='Number of steps to an epoch')
     training_group.add_argument('--params', default='./params', help='Location for storing plot files')
-    training_group.add_argument('--lr', type=float, default=0.001, help='Learning Rate')
+    training_group.add_argument('--lr', type=float, default=0.01, help='Learning Rate')
+    training_group.add_argument('--lr_start_factor', type=float, default=1.0, help='Learning Rate start factor')
+    training_group.add_argument('--lr_end_factor', type=float, default=0.01, help='Learning Rate end factor')
+    training_group.add_argument('--total_lr_iters', default=None, type=int, help='Number of steps to fully decay learning rate')
     training_group.add_argument('--weight_decay', type=float, default=1e-5, help='Weight decay')
     training_group.add_argument('--momentum', type=float, default=0, help='Momentum for SGD')
     training_group.add_argument('--optimizer', choices=OptimizerFactory.choices, default=OptimizerFactory.get_default(),
@@ -122,7 +126,7 @@ def parse_args(factory):
     training_group.add_argument('--restart', default=None, help='Restart from saved parameters')
     training_group.add_argument('--width', default=28, type=int, help='Width of each image in pixels')
     training_group.add_argument('--height', default=28, type=int, help='Height of each image in pixels')
-    training_group.add_argument('--bottleneck', default=3, type=int, help='Number of neurons in bottleneck')
+    training_group.add_argument('--bottleneck', default=14, type=int, help='Number of neurons in bottleneck')
 
     test_group = parser.add_argument_group('Parameters for --action test')
     test_group.add_argument('--file', default=None, help='Used to load weights')
@@ -153,6 +157,7 @@ def encoder_training_step(model, batch, optimizer):
     optimizer.step()
     optimizer.zero_grad()
 
+
 def get_validation_loss(model, loader):
     '''
     Used during traning to compute loss from validation data
@@ -162,6 +167,7 @@ def get_validation_loss(model, loader):
         loader    Source for batches of data
     '''
     return [float(model.get_batch_loss(batch).detach()) for batch in loader]
+
 
 def get_file_name(args):
     '''
@@ -186,6 +192,7 @@ def generate_samples(images, n=12):
 
     for image_index in range(n):
         yield samples[image_index]
+
 
 def display_images(model, loader, bottleneck, nrows=4, ncols=2, fig=None):
     '''
@@ -216,7 +223,8 @@ def display_images(model, loader, bottleneck, nrows=4, ncols=2, fig=None):
 
     batch_number = rng.choice(len(loader) - 1)
     for k, batch in enumerate(loader):
-        if k != batch_number: continue
+        if k != batch_number:
+            continue
         images, _ = batch
         processed = model(images)
         samples = generate_samples(images, n=nrows * ncols)
@@ -233,7 +241,7 @@ def display_images(model, loader, bottleneck, nrows=4, ncols=2, fig=None):
         return
 
 
-def plot_losses(history, ax=None, bottleneck=3, window_size=11,optimizer_text=''):
+def plot_losses(history, lr_history, ax=None, bottleneck=3, window_size=11, optimizer_text=''):
     '''
     Plot history plus moving average
 
@@ -250,7 +258,13 @@ def plot_losses(history, ax=None, bottleneck=3, window_size=11,optimizer_text=''
     ax.set_ylabel('Loss')
     ax.set_xlabel('Step')
     ax.set_ylim(bottom=0)
-
+    ax_twin = ax.twinx()
+    ax_twin.plot(xs,lr_history,c='xkcd:green',label='Learning Rate')
+    ax_twin.set_ylim(bottom=0)
+    ax_twin.set_ylabel('Learning Rate')
+    legend_handles1, legend_labels1 = ax.get_legend_handles_labels()
+    legend_handles2, legend_labels2 = ax_twin.get_legend_handles_labels()
+    ax.legend(legend_handles1 + legend_handles2, legend_labels1 + legend_labels2, loc='upper right')
 
 def display_manifold(auto_encoder, loader, fig):
     '''
@@ -288,6 +302,12 @@ def display_manifold(auto_encoder, loader, fig):
     ax.legend(sorted_handles, sorted_labels, title='Labels', loc='upper right', markerscale=3)
 
 
+def create_scheduler(optimizer, args):
+    total_iters = args.total_lr_iters if args.total_lr_iters != None else args.N // 2
+    return LinearLR(optimizer, start_factor=args.lr_start_factor,
+                    end_factor=args.lr_end_factor, total_iters=total_iters)
+
+
 if __name__ == '__main__':
     rc('font', **{'family': 'serif',
                   'serif': ['Palatino'],
@@ -304,25 +324,26 @@ if __name__ == '__main__':
     match args.action:
         case 'train1':
             auto_encoder = auto_encoder_factory.create(args)
-            optimizer,optimizer_text = OptimizerFactory.create(auto_encoder, args)
-            scheduler = LinearLR(optimizer, start_factor=1.0, end_factor=0.5, total_iters=args.N//2)
+            optimizer, optimizer_text = OptimizerFactory.create(auto_encoder, args)
+            scheduler = create_scheduler(optimizer, args)
             dataset = MNIST(root=args.data, download=True, transform=tr.ToTensor())
             train_data, validation_data = random_split(dataset, [50000, 10000])
             train_loader = DataLoader(train_data, args.batchsize, shuffle=True)
             validation_loader = DataLoader(validation_data, args.batchsize, shuffle=False)
             history = []
+            lr_history = []
             for epoch in range(args.N):
                 for _ in range(args.n):
                     for batch in train_loader:
                         encoder_training_step(auto_encoder, batch, optimizer)
 
-                before_lr = optimizer.param_groups[0]["lr"]
+                lr_previous = optimizer.param_groups[0]["lr"]
                 scheduler.step()
-                after_lr = optimizer.param_groups[0]["lr"]
 
                 validation_losses = get_validation_loss(auto_encoder, validation_loader)
                 history += validation_losses
-                print(f'Epoch {epoch + 1} of {args.N}. Loss = {np.mean(validation_losses)}, lr={before_lr}->{after_lr}')
+                lr_history += len(validation_losses) * [lr_previous]
+                print(f'Epoch {epoch + 1} of {args.N}. Loss = {np.mean(validation_losses):.6f}, lr={lr_previous:.6f}')
 
                 checkpoint_file_name = join(args.params, get_file_name(args))
                 ensure_we_can_save(checkpoint_file_name)
@@ -331,7 +352,7 @@ if __name__ == '__main__':
                     break
 
             subfigs = fig.subfigures(2, 1, wspace=0.07)
-            plot_losses(history, ax=subfigs[0].add_subplot(1, 1, 1),
+            plot_losses(history, lr_history, ax=subfigs[0].add_subplot(1, 1, 1),
                         bottleneck=args.bottleneck, optimizer_text=optimizer_text)
             display_manifold(auto_encoder, validation_loader, fig=subfigs[1])
             fig.savefig(join(args.figs, get_file_name(args)))
@@ -341,21 +362,29 @@ if __name__ == '__main__':
             auto_encoder.load('./params/aetrain.pth')
             perceptron = Perceptron()
             optimizer = OptimizerFactory.create(auto_encoder, args)
+            scheduler = create_scheduler(optimizer, args)
             dataset = MNIST(root=args.data, download=True, transform=tr.ToTensor())
             train_data, validation_data = random_split(dataset, [50000, 10000])
             train_loader = DataLoader(train_data, args.batchsize, shuffle=True)
             validation_loader = DataLoader(validation_data, args.batchsize, shuffle=False)
             history = []
+            lr_history = []
+
             for epoch in range(args.N):
                 for _ in range(args.n):
                     for batch in train_loader:
                         perceptron.training_step(batch, auto_encoder, optimizer)
 
-                    validation_losses = get_validation_loss(auto_encoder, validation_loader)
-                    print(f'Epoch {epoch + 1} of {args.N}. Average validation loss = {np.mean(validation_losses)}')
-                    history += validation_losses
+                    lr_previous = optimizer.param_groups[0]["lr"]
+                    scheduler.step()
 
-            plot_losses(history, ax=fig.add_subplot(1, 1, 1))
+                    validation_losses = get_validation_loss(auto_encoder, validation_loader)
+                    history += validation_losses
+                    lr_history += len(validation_losses) * [lr_previous]
+                    print(f'Epoch {epoch + 1} of {args.N}. Loss = {np.mean(validation_losses):.6f}, lr={lr_previous:.6f}')
+
+
+            plot_losses(history, lr_history, ax=fig.add_subplot(1, 1, 1))
 
         case 'test':
             auto_encoder = auto_encoder_factory.create(args)
@@ -363,7 +392,7 @@ if __name__ == '__main__':
             dataset = MNIST(root=args.data, download=True, train=False, transform=tr.ToTensor())
             loader = DataLoader(dataset, args.batchsize)
             display_images(auto_encoder, loader, args.bottleneck, nrows=args.nrows, ncols=args.ncols, fig=fig)
-            fig.savefig(join(args.figs,f'{Path(args.file).stem}-test'))
+            fig.savefig(join(args.figs, f'{Path(args.file).stem}-test'))
 
         case 'plot_encoded':
             auto_encoder = auto_encoder_factory.create(args)
