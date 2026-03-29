7843d83b52714b9650d0c1ce1db9dcf643103389
Simon Crase <simon@greenweaves.nz>
2026-03-29 16:21:31 +1300

    #98 Created program to generate and fit data

---
+++ bt.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2026 Simon Crase  simon@greenweaves.nz
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+'''Bradley Terry'''
+
+
+from argparse import ArgumentParser
+from pathlib import Path
+from matplotlib.pyplot import figure,show
+import numpy as np
+from scipy.stats import linregress
+
+def generate_parameters(m,rng = np.random.default_rng()):
+    '''
+    Generate Bradley Terry parameters
+    
+    Parameters:
+        m       Number of parameters
+        rng
+    '''
+    P = rng.uniform(size=m)
+    return P / P.sum()
+
+
+def generate_contests(n,P,rng = np.random.default_rng()):
+    m = len(P)
+    Pairwise = np.zeros((m,m))
+    for i in range(m):
+        for j in range(m):
+            if i != j:
+                Pairwise[i,j] = P[i]/(P[i] + P[j])
+    indices = rng.permutation(m).reshape(m//2,2)
+    Contests = np.zeros((n,(m//2),3),dtype=int)
+    for k in range(n):
+        Contests[k,:,0:2] = rng.permutation(m).reshape(m//2,2)
+    Contests = Contests.reshape(-1, Contests.shape[-1])
+    for k in range(n *(m//2)):
+        i = Contests[k,0] 
+        j = Contests[k,1]         
+        assert i != j 
+        Contests[k,2] = i if rng.uniform() < Pairwise[i,j] else j
+ 
+    return Contests
+    
+def bt(Contests,m,N):
+    '''
+    Bradley-Terry
+    '''
+    def createW():
+        W = np.zeros((m,m),dtype=int)
+        n,_ = Contests.shape
+        for k in range(n):
+            i = Contests[k,0]
+            j = Contests[k,1]
+            if Contests[k,2] == i:     # i wins
+                W[i,j] += 1
+            elif Contests[k,2] == j:   # j wins
+                W[j,i] += 1
+            else:
+                raise RuntimeError(f'k={k}, i={i}, j={j}, Outcome mismatch: {Contests[k,2]}')
+            
+        return W,np.sum(W,axis=1)
+
+    def create_p():
+        p = np.zeros((N+1,m))
+        p[0,:] = rng.uniform(size=m)
+        p[0,:] /= p[0,:].sum()
+        return p
+    
+    W,w = createW()
+    p = create_p()
+    
+    for k in range(N):
+        for i in range(m):
+            wp = 0
+            for j in range(m):
+                if i != j:
+                    wp += (W[i,j] + W[j,i]) / (p[k,i] + p[k,j])
+                    
+            p[k+1,i] = w[i]/wp
+            
+        Z = p[k+1,:].sum()
+        p[k+1,:] /= Z
+        
+    return p
+                  
+
+def parse_args():
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be displayed')
+    parser.add_argument('--figs', default='./figs', help='Location for storing plot files')
+    parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--m', type=int, default=12,help='Number of Players')
+    parser.add_argument('--n', type=int, default=100,help='Number of rounds')
+    parser.add_argument('--N', type=int, default=100,help='Number of Iterations')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    rng = np.random.default_rng(args.seed)
+    P = generate_parameters(args.m,rng=rng)
+    Contests = generate_contests(args.n,P,rng=rng)
+    p = bt(Contests,args.m,args.N)
+    slope, intercept, r, pvalue, se = linregress(P,p[-1,:])
+    
+    fig = figure(figsize=(12,12))
+    ax1 = fig.add_subplot(1,2,1)
+    for k in range(args.m):
+        ax1.plot(p[:,k])
+    ax1.set_title('Evolution of Bradley-Terry Parameters')
+    ax1.set_xlabel('Iteration')
+    ax1.set_ylabel('P')
+
+    ax2 = fig.add_subplot(1,2,2)
+    ax2.scatter(P,p[-1,:],c='xkcd:blue',label='Calculated')
+    P0 = np.sort(P)
+    ax2.plot(P0,slope*P0+intercept,c='xkcd:red',label=f'Slope={slope:.3f}, intercept={intercept:.3f}')
+    ax2.set_title(f'r={r:.3f}, pvalue={pvalue:.3e}, se={se:.3e}')
+    ax2.set_xlabel('Ground Truth')
+    ax2.set_ylabel('Calculated')
+    ax2.legend()
+    
+    fig.suptitle(f'Number of Players={args.m:,}, Number of Rounds={args.n:,}, Number of Iterations={args.N:,}')
+    
+    if args.show:
+        show()
