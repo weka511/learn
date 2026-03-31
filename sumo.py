e00176a05aa706ba1fb3f51820f3cd30d18231be
Simon Crase <simon@greenweaves.nz>
2026-03-31 18:08:27 +1300

    #98 Plot evolution with labels

--- sumo.py
+++ sumo.py
@@ -27,70 +27,97 @@ from matplotlib.pyplot import figure,show
 import numpy as np
 from bt import bt
 
-def parse_args():
-    parser = ArgumentParser(__doc__)
-    parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be displayed')
-    parser.add_argument('--figs', default='./figs', help='Location for storing plot files')
-    parser.add_argument('--seed', type=int, default=None)
-    parser.add_argument('--N', type=int, default=50,help='Number of Iterations')
-    parser.add_argument('--burn', type=int, default=0,help='Burn in: skip this many iterations when we display')
-    parser.add_argument('out',  help='Name of plot file')
-    parser.add_argument('--data',default = './data/sumo')
-    parser.add_argument('--year',type=int,default=2019)
-    parser.add_argument('--epsilon',type=float,default=1.0e-6)
-    return parser.parse_args()
-
 class Rikishi:
     next_seq = 0
+    
     def __init__(self,rikishi_id,rank,shikona):
         self.seq = Rikishi.next_seq
         self.rikishi_id = rikishi_id
         self.rank = rank
         self.shikona = shikona
         Rikishi.next_seq += 1
+        
+    def __str__(self):
+        return f'{self.seq} {self.rikishi_id} {self.rank} {self.shikona}'
     
 class Results:
+    INDEX = 0
+    BASHO = 1
+    DAY = 2
+    RIKISHI_1_ID = 3
+    RIKISHI_1_RANK = 4
+    RIKISHI_1_SHIKONA = 5
+    RIKISHI_1_RESULT = 6
+    RIKISHI_1_WIN = 7
+    KIMARITE = 8
+    RIKISHI_2_ID = 9
+    RIKISHI_2_RANK = 10
+    RIKISHI_2_SHIKONA = 11
+    RIKISHI_2_RESULT = 12
+    RIKISHI_2_WIN = 13    
+    
     def __init__(self):
-        self.rishiki = {}
+        self.rikishi = {}
+        self.rikishi_by_seq = {}
         
-    def get_rishiki(self,rikishi_id,rank,shikona):
-        if rikishi_id not in self.rishiki:
-            self.rishiki[rikishi_id] = Rikishi(rikishi_id,rank,shikona)
-        return self.rishiki[rikishi_id]     
+    def get_rikishi(self,rikishi_id,rank,shikona):
+        if rikishi_id not in self.rikishi:
+            rikishi =  Rikishi(rikishi_id,rank,shikona)
+            self.rikishi[rikishi_id] = rikishi
+            self.rikishi_by_seq[rikishi.seq] =  rikishi
+        return self.rikishi[rikishi_id]     
         
-# 0      1      2  3           4             5                6               7             8      9
-# index,basho,day,rikishi1_id,rikishi1_rank,rikishi1_shikona,rikishi1_result,rikishi1_win,kimarite,rikishi2_id
-#      10            11              12            13
-# ,rikishi2_rank,rikishi2_shikona,rikishi2_result,rikishi2_win
     def build(self,path):
         Product = []
         with (open(path)) as file:
             for i,line in enumerate(file):
                 if i == 0: continue
                 fields = line.strip().split(',')
-                basho = fields[0]
-                rikishi_1 = self.get_rishiki(fields[3],fields[4],fields[5])
-                rikishi_2 = self.get_rishiki(fields[9],fields[10],fields[11])
-                if int(fields[7]) == 1 and int(fields[13]) == 0:
+                basho = fields[Results.BASHO]
+                rikishi_1 = self.get_rikishi(fields[Results.RIKISHI_1_ID],
+                                             fields[Results.RIKISHI_1_RANK],
+                                             fields[Results.RIKISHI_1_SHIKONA])
+                rikishi_2 = self.get_rikishi(fields[Results.RIKISHI_2_ID],
+                                             fields[Results.RIKISHI_2_RANK],
+                                             fields[Results.RIKISHI_2_SHIKONA])
+                if int(fields[Results.RIKISHI_1_WIN]) == 1 and int(fields[Results.RIKISHI_2_WIN]) == 0:
                     Product.append([rikishi_1.seq,rikishi_2.seq,rikishi_1.seq])
-                elif int(fields[13]) == 1 and int(fields[7]) == 0:
+                elif int(fields[Results.RIKISHI_2_WIN]) == 1 and int(fields[Results.RIKISHI_1_WIN]) == 0:
                     Product.append([rikishi_1.seq,rikishi_2.seq,rikishi_2.seq])
                 else:
                     raise RuntimeError(str(i))
+               
         return np.array(Product)
 
+def parse_args():
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--show', default=False, action='store_true', help='Controls whether plot will be displayed')
+    parser.add_argument('--figs', default='./figs', help='Location for storing plot files')
+    parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--N', type=int, default=150,help='Number of Iterations')
+    parser.add_argument('--burn', type=int, default=0,help='Burn in: skip this many iterations when we display')
+    parser.add_argument('out',  help='Name of plot file')
+    parser.add_argument('--data',default = './data/sumo',help='Path to data files')
+    parser.add_argument('--year',type=int,default=2019,help='Year to be processed')
+    parser.add_argument('--epsilon',type=float,default=1.0e-6,help="Correction for Cromwell's rule")
+    parser.add_argument('--cutoff',type=int, default=10,help='Controls how many labels will be displayed in plot')
+    return parser.parse_args()
+    
 if __name__ == '__main__':
     args = parse_args()
     rng = np.random.default_rng(args.seed)
     results = Results()
     Contests = results.build((Path(args.data) / str(args.year)).with_suffix('.csv'))
     scores = bt(Contests,Rikishi.next_seq,args.N,epsilon=0.001)
-    
+    indices = np.argsort(scores[-1,:])[::-1][0:args.cutoff]
     fig = figure(figsize=(12,12))
     ax1 = fig.add_subplot(1,1,1)
     for k in range(Rikishi.next_seq):
-        ax1.plot(scores[args.burn:,k])
-    ax1.set_title('Evolution of Bradley-Terry Parameters')
+        ax1.plot(scores[args.burn:,k],
+                 linestyle='solid' if k in indices else 'dotted',
+                 label=results.rikishi_by_seq[k] if k in indices else None)
+    ax1.legend(loc='center left')
+    ax1.set_title(f'Evolution of Bradley-Terry Parameters for Bashos in {args.year}' )
     ax1.set_xlabel('Iteration')
     ax1.set_ylabel('P')
     ax1.set_xbound(args.burn,args.N)  
