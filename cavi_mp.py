44d0f76e5bf8ed2263b20226a11b938ade0b9f20
Simon Crase <simon@greenweaves.nz>
2026-04-24 21:46:10 +1200

    #123 Plot results

--- cavi_mp.py
+++ cavi_mp.py
@@ -33,7 +33,7 @@ from tempfile import gettempdir
 import numpy as np
 from shared.utils import generate_xkcd_colours,Splitter
 from gmm import GaussionMixtureModel, get_name, create_colours
-from cavi_nd import Solution, initialize, get_updated_assignments, get_updated_statistics, get_ELBO
+from cavi_nd import Solution, initialize, get_updated_assignments, get_updated_statistics, get_ELBO,create_data_colours
 
 def parse_args():
     parser = ArgumentParser(__doc__)
@@ -54,7 +54,7 @@ def parse_args():
   
 def cavi_run(run_number:int,x_train,x_test,K,N,rng,solution,path,file_name,BURN_IN,atol):
     print (f'Run {run_number}')
-
+    dummy = rng.integers(0, 100, size=K*run_number+7)
     m, s, c = initialize(x_train, K, rng=rng)
     solution.accumulateELBO(get_ELBO(m, s, c, x_train)) 
     for j in range(N):
@@ -90,26 +90,87 @@ def run_processes(args,x_train,x_test,rng=np.random.default_rng(),prefix='foo'):
             
         for i in range(len(processes)):  
             processes[i].join()
+            
+def create_solutions(temp_path,prefix,M):
+    Solutions = [Solution.create(get_solution_path(temp_path,prefix,i)) for i in range(M)]
+    ELBOS = [solution.ELBO[-1] for solution in Solutions]
+    return np.argmax(ELBOS),Solutions
 
+def plotELBOs(index_best,Solutions,ax=None):
+    ELBO_colours = generate_xkcd_colours()
+    for i in range(len(Solutions)):
+        label = None
+        linestyle = 'dotted'
+        if i == index_best:
+            label = f'best {Solutions[i].ELBO[-1]:.6}'
+            linestyle = 'solid'
+        ax.plot(range(len(Solutions[i].ELBO)),Solutions[i].ELBO, c=next(ELBO_colours), label=label, linestyle=linestyle)
+    ax.legend()
+    ax.set_title(f'ELBO for {len(Solutions)} runs, plotted with held-out data')
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('ELBO')
+    
 def main():
     start = time()
     args = parse_args()
     rng = np.random.default_rng(args.seed)
     temp_path = gettempdir()
     prefix = basename(__file__).split('.')[0]
-    ELBO_colours = generate_xkcd_colours()
+    
     model = GaussionMixtureModel()
     path_name = Path(args.path) / args.name
     
     splitter = Splitter(rng=rng,test_size=args.test)
     x_train,x_test = splitter.split(model.load(path_name.with_suffix('.npz')))    
     run_processes(args,x_train,x_test,rng=rng,prefix=prefix)
-    for i in range(args.M):
-        solution = Solution.create(get_solution_path(temp_path,prefix,i))
+       
+    index_best,Solutions = create_solutions(temp_path,prefix,args.M)
+    
+    fig = figure(figsize=(12, 12))
+    fig.suptitle(f'{args.name}')
+    plotELBOs(index_best,Solutions,ax=fig.add_subplot(2, 1, 1))
+    
+    _,d = x_train.shape
+    match d:
+        case 1:
+            ax2 = fig.add_subplot(2, 1, 2)
+            n, _, _ = ax2.hist(x_test, bins='sturges', color='xkcd:blue', label='x',density=True)
+            ax2.vlines(np.ravel(Solutions[index_best].m), 0, max(n), 
+                       colors='xkcd:red', linestyles='dashed', label='Means (fitted)')
+            ax2.set_xlabel('X')
+            ax2.set_ylabel('p')
+            ax2.legend()
+
+        case 2:
+            ax2 = fig.add_subplot(2, 1, 2)
+            ax2.scatter(x_test[:, 0], x_test[:, 1],
+                        c=create_data_colours(x_test, Solutions[index_best].c_test, create_colours(args.K)), s=1)
+            for k in range(args.K):
+                ax2.scatter(Solutions[index_best].m[k, 0], Solutions[index_best].m[k, 1],
+                            c='xkcd:black', marker='+', s=25)
+            ax2.set_title(f'Solution with best ELBO: {Solutions[index_best].ELBO[-1]:.6} after {args.M} runs')
+            ax2.set_xlabel('X')
+            ax2.set_ylabel('Y')
+
+        case 3:
+            ax2 = fig.add_subplot(2, 1, 2,projection='3d')
+            ax2.scatter(x_test[:,0],x_test[:,1],x_test[:,2],
+                        c=create_data_colours(x_test, Solutions[index_best].c_test, create_colours(args.K)), s=1)
+            ax2.set_title(f'Solution with best ELBO: {Solutions[index_best].ELBO[-1]:.6} after {args.M} runs')
+            ax2.set_xlabel('X')
+            ax2.set_ylabel('Y')
+            ax2.set_zlabel('Z')    
+        
+    fig.tight_layout(pad=3,h_pad=4)
+    figs_path_name = Path(args.figs) / args.name
+    fig.savefig(figs_path_name.with_suffix('.png'))    
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
-    print (f'Elapsed Time {minutes} m {seconds:.2f} s')    
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s') 
+    if args.show:
+        show()    
+    
     
 if __name__ == '__main__':
     main()
