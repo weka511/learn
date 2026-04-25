9ebcd2d4e3cf9e4eaf3f872b33cb63ff99c5556b
Simon Crase <simon@greenweaves.nz>
2026-04-25 20:42:22 +1200

    #124 Pipeline woring, but plot produces error

--- cavi_w.py
+++ cavi_w.py
@@ -28,14 +28,15 @@ from os import remove
 from os.path import basename
 from pathlib import Path
 from time import time
-from matplotlib.pyplot import figure, rcParams, show
+from matplotlib.pyplot import figure, rcParams, show,subplot
 from multiprocessing import cpu_count, Process, Pool
 from tempfile import gettempdir
 import numpy as np
 from shared.utils import generate_xkcd_colours, Splitter
 from gmm import GaussionMixtureModel, get_name, create_colours
 import cavi_nd as cavi
-
+import matplotlib
+matplotlib.use('Agg')
 def parse_args():
     '''
     Parse command line argumengts
@@ -56,15 +57,66 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def foo(n):
-    return n**2
+def create_solution(x_train,x_test,K,rng = np.random.default_rng()):
+    Product = cavi.Solution()
+    m, s, c = cavi.initialize(x_train, K, rng=rng)
+    Product.set_params(m,s,c,[])
+    Product.accumulateELBO(cavi.get_ELBO(m, s, c, x_train))
+    return Product
+
+
+
+class Explorer:
+    def __init__(self,x_train, x_test, K, N, BURN_IN, atol):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.K = K
+        self.N = N
+        self.BURN_IN = BURN_IN
+        self.atol = atol
+    
+    def explore(self,solution):
+        m = solution.m
+        s = solution.s
+    
+        for j in range(self.N):
+            c = cavi.get_updated_assignments(m, s, self.x_train)
+            m, s = cavi.get_updated_statistics(m, s, c, self.x_train)
+            c_test = cavi.get_updated_assignments(m, s, self.x_test)
+            solution.accumulateELBO(cavi.get_ELBO(m, s, c_test, self.x_test))
+            if j > self.BURN_IN and solution.ELBO[-1] - solution.ELBO[-2] < self.atol:
+                break
+    
+        solution.set_params(m, s, c, c_test)        
+        return solution
 
 def main():
+    start = time()
     args = parse_args()
-    inputs = list(range(100))
-    with Pool(processes=cpu_count()) as pool:
-        results = pool.map(foo, inputs)
-    print( results)    
+    rng = np.random.default_rng(args.seed)
+    model = GaussionMixtureModel()
+    path_name = Path(args.path) / args.name
+
+    splitter = Splitter(rng=rng, test_size=args.test)
+    x_train, x_test = splitter.split(model.load(path_name.with_suffix('.npz')))
+    
+    explorer = Explorer(x_train, x_test, args.K, args.N, args.BURN_IN, args.atol)
+    Solutions0 = [create_solution(x_train,x_test,args.K,rng=rng) for _ in range(args.M)]
+    with Pool(processes=cpu_count()-1) as pool:
+        Solutions = pool.map(explorer.explore, Solutions0)
+    ELBOS = [solution.ELBO[-1] for solution in Solutions]
+    index_best = np.argmax(ELBOS)
+    print (len(ELBOS),ELBOS[index_best])
+    print (ELBOS)
+    #fig = figure(figsize=(12, 12))
+    #fig.suptitle(f'{args.name}')
+    #cavi.plotELBOs(index_best, Solutions, ax=fig.add_subplot(2, 1, 1))   
+    elapsed = time() - start
+    minutes = int(elapsed / 60)
+    seconds = elapsed - 60 * minutes
+    print(f'Elapsed Time {minutes} m {seconds:.2f} s')
+    if args.show:
+        show()    
 
 if __name__ == '__main__':
     main()
