42d1d6e92538a63385ae1334e95a565ffcfbb094
Simon Crase <simon@greenweaves.nz>
2026-05-01 19:29:52 +1200

    #128 Plot with legends

--- bnp-dirichlet.py
+++ bnp-dirichlet.py
@@ -37,18 +37,41 @@ def parse_args():
     parser.add_argument('--seed', default=None, type=int, help='Used to initialize random number generator')
     parser.add_argument('--data', default='./data', help='Location of data files')
     parser.add_argument('--N', type=int, default=1000, help='Number of samples')
-    parser.add_argument('--alpha', default=[10.0,1.0,0.1],nargs='+',help='Parameters for Dirichlet Distribution')
+    parser.add_argument('--alpha', default=[1000.0,1.0,0.001],nargs='+',help='Parameters for Dirichlet Distribution')
 
     return parser.parse_args()
 
-def plot(samples,alpha=[0.1,0.1,0.1]):
+def plot(samples,m=2,n=2,alpha=[0.1,0.1,0.1],figs='./figs'):
     fig = figure(figsize=(8, 8))
-    ax = fig.add_subplot(1,1,1)
+    k = 0
     colours = generate_xkcd_colours()
-    for j in range(len(alpha)):
-        ax.hist(samples[:,j],color=next(colours),label=r'$\alpha$'+f'[{alpha[j]}]',alpha=0.5)
-    ax.legend()
-    
+    lines = None
+    labels = None
+    for i in range(m):
+        for j in range(n):
+            if k >= len(alpha): break
+            ax = fig.add_subplot(m,n,k+1)
+            ax.hist(samples[:,k],color=next(colours),label=r'$\alpha$'+f'[{alpha[k]}]',bins=10)
+            if k == 0:
+                lines, labels = ax.get_legend_handles_labels()
+            else:
+                lines1, labels1 = ax.get_legend_handles_labels()
+                lines = lines + lines1
+                labels = labels + labels1
+            k += 1
+            
+    ax.legend(lines,labels)
+    fig.tight_layout(pad=3,h_pad=4)
+    figs_path_name = (Path(figs) / Path(__file__).stem).with_suffix('.png')
+    fig.savefig(figs_path_name.with_suffix('.png'))    
+
+def get_rows_and_columns(M):
+    m = int(np.sqrt(M))
+    n = M // m
+    while m*n < M:
+        n += 1
+    return m,n
+
 def main():
     rc('font', **{'family': 'serif',
                   'serif': ['Palatino'],
@@ -58,9 +81,10 @@ def main():
     start = time()
     args = parse_args()
     rng = np.random.default_rng(args.seed)
-    
-    plot(samples = rng.dirichlet(args.alpha,size=args.N))
 
+    m,n = get_rows_and_columns(len(args.alpha))   
+    plot(samples = rng.dirichlet(args.alpha,size=args.N),m=m,n=n,alpha=args.alpha,figs=args.figs)
+ 
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
