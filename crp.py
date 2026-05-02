749631f0bda98a9f44bbe4eb79aa01fb91a9e373
Simon Crase <simon@greenweaves.nz>
2026-05-02 12:56:10 +1200

    #131 Plot closters if d==2

--- crp.py
+++ crp.py
@@ -24,17 +24,19 @@ from time import time
 from matplotlib.pyplot import figure, show
 from matplotlib import rc
 import numpy as np
-from shared.utils import Logger
+from shared.utils import Logger,generate_xkcd_colours
 
 class Table:
     mu0 = 0
     sigma0 = 1
- 
+    colours = generate_xkcd_colours()
+    
     def __init__(self,rng=np.random.default_rng()):
         self.rng = rng
         self.mu = rng.normal(loc=Table.mu0,scale=Table.sigma0)
-        self.sigma = Table.sigma0
+        self.sigma = 0.25*Table.sigma0   #FIXME
         self.indices = []
+        self.colour = next(Table.colours)
         
     def __len__(self):
         return len(self.indices)
@@ -56,7 +58,7 @@ def parse_args():
     parser.add_argument('--N', type=int, default=1000, help='Number of samples')
     parser.add_argument('--dimensionality', '-d',type=int, default=3, help='Dimensionality')
     parser.add_argument('--alpha', default=0.1,type=float,help='Parameter for CRP')
-    parser.add_argument('--mu', default=[0,0,0],type=float,help='Parameter for CRP')
+    parser.add_argument('--mu', default=[0,0,0],type=float,nargs='+',help='Parameter for CRP')
     parser.add_argument('--sigma', default=1,type=float,help='Parameter for CRP')
     
     return parser.parse_args()
@@ -84,27 +86,40 @@ def main():
     Table.sigma0 = args.sigma
  
     tables = []
-    tables.append(Table(rng))
-    z[0] = tables[-1].get_sample()
-    tables[-1].append(0)
-    for i in range(1,args.N):
-        
-        index = rng.choice(len(tables)+1,p=create_weights(tables,args.alpha))
+    for i in range(args.N):        
+        index = rng.choice(len(tables)+1,
+                           p=create_weights(tables,args.alpha))
         if index == len(tables):
             tables.append(Table(rng))
         z[i] = tables[index].get_sample()
-        tables[index].append(z[i])
- 
+        tables[index].append(i)
     
     output_file = (Path(args.data) / args.out).with_suffix('.npz')
     np.savez(output_file,z=z)
+    print (f'Saved {args.N} points in {len(tables)} clusters to {output_file}')
     
     fig = figure(figsize=(8, 8))
-    ax = fig.add_subplot(1,1,1)
-    ax.bar(range(len(tables)),[len(table) for table in tables],color='xkcd:blue')
-    ax.set_xlabel('Tables')
-    ax.set_ylabel('Number')
-    ax.set_title(r'$\alpha=$'+f'{args.alpha}')
+    ax1 = fig.add_subplot(2,1,1)
+    ax1.bar(range(len(tables)),[len(table) for table in tables],
+            label=[f'{i}' for i in range(len(tables))],
+            color=[table.colour for table in tables])
+    ax1.set_xlabel('Tables')
+    ax1.set_ylabel('Number')
+    ax1.set_title(r'$\alpha=$'+f'{args.alpha}')
+    ax1.legend(title='Clusters',ncols=int(np.sqrt(len(tables))))
+    match args.dimensionality:
+        case 1:
+            pass
+        case 2:
+            ax2 = fig.add_subplot(2,1,2)
+            for i,table in enumerate(tables):
+                indices = table.indices
+                ax2.scatter(z[indices,0],z[indices,1],
+                            c=table.colour,label=f'{i}')
+ 
+        case 3:
+            pass
+        
     fig.tight_layout(pad=3,h_pad=4)
     figs_path_name = (Path(args.figs) / Path(__file__).stem).with_suffix('.png')
     fig.savefig(figs_path_name.with_suffix('.png'))    
